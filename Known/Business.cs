ea6ca2cb18e1720c3c77a6ce27158a6db8f932ae
Known Chen <knownchen@163.com>
2018-01-20 21:20:16 +0800

    add business

---
+++ Known/Business.cs
@@ -0,0 +1,17 @@
+﻿namespace Known
+{
+    public class Business
+    {
+        public Business(Context context)
+        {
+            Context = context;
+        }
+
+        public Context Context { get; }
+
+        protected T LoadBusiness<T>() where T : Business
+        {
+            return BusinessFactory.Create<T>(Context);
+        }
+    }
+}
---
+++ Known/BusinessFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Known
+{
+    public class BusinessFactory
+    {
+        private static Hashtable cached = new Hashtable();
+
+        public static T Create<T>(Context context) where T : Business
+        {
+            var key = typeof(T);
+            if (!cached.ContainsKey(key))
+            {
+                lock (cached.SyncRoot)
+                {
+                    if (!cached.ContainsKey(key))
+                    {
+                        cached[key] = Activator.CreateInstance(typeof(T), context);
+                    }
+                }
+            }
+
+            return (T)cached[key];
+        }
+    }
+}
