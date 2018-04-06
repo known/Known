67c2c3b1671a397ce20b78c20e3f3ea7b3feb0e2
knownchen <knownchen@163.com>
2018-04-06 11:12:22 +0800

    add ImageOption

--- Known/Helpers/ImageHelper.cs
+++ Known/Helpers/ImageHelper.cs
@@ -7,37 +7,6 @@ using System.IO;
 namespace Known.Helpers
 {
     /// <summary>
-    /// 图片参数选项。
-    /// </summary>
-    public class ImageOption
-    {
-        /// <summary>
-        /// 取得或设置X位置。
-        /// </summary>
-        public int X { get; set; }
-
-        /// <summary>
-        /// 取得或设置Y位置。
-        /// </summary>
-        public int Y { get; set; }
-
-        /// <summary>
-        /// 取得或设置宽度。
-        /// </summary>
-        public int Width { get; set; }
-
-        /// <summary>
-        /// 取得或设置高度。
-        /// </summary>
-        public int Height { get; set; }
-
-        /// <summary>
-        /// 取得或设置图片水印文本。
-        /// </summary>
-        public string WatermarkText { get; set; }
-    }
-
-    /// <summary>
     /// 图片帮助者。
     /// </summary>
     public sealed class ImageHelper
