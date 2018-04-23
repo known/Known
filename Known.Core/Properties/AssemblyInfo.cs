b257bb26805a3e9074f77290fae6a10c1eacc01d
knownchen <knownchen@163.com>
2018-04-23 21:33:36 +0800

    Add Known.Core

---
+++ Known.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Known.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Known.Core")]
+[assembly: AssemblyCopyright("Copyright © Known 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2d58247b-c669-4b8c-8909-fb7c92742dfb")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
