d20610f8cb65624b4bd273156222e9d4e44de3d0
knownchen <knownchen@163.com>
2018-04-29 10:48:40 +0800

    Add Test for DefaultCache

--- Known.Tests/KnownTests/Cache/DefaultCacheTest.cs
+++ Known.Tests/KnownTests/Cache/DefaultCacheTest.cs
@@ -4,13 +4,39 @@ namespace Known.Tests.KnownTests.Cache
 {
     public class DefaultCacheTest
     {
+        private static ICache cache = new DefaultCache();
+
         public static void TestGet()
         {
-            var cache = new DefaultCache();
             cache.Set("Key", "Value");
             Assert.IsNull(cache.Get(""));
             Assert.IsNull(cache.Get("test"));
             Assert.IsEqual(cache.Get("Key"), "Value");
+        }
+
+        public static void TestSet()
+        {
+            cache.Set("Key", null);
+            Assert.IsNull(cache.Get("Key"));
+            cache.Set("Key", "Value");
+            Assert.IsEqual(cache.Get("Key"), "Value");
+        }
+
+        public static void TestRemove()
+        {
+            cache.Set("Key", "Value");
+            Assert.IsEqual(cache.Get("Key"), "Value");
+            cache.Remove("Key");
+            Assert.IsNull(cache.Get("Key"));
+        }
+
+        public static void TestRemoveAll()
+        {
+            cache.Set("Key1", "Value1");
+            cache.Set("Key2", "Value2");
+            cache.RemoveAll();
+            Assert.IsNull(cache.Get("Key1"));
+            Assert.IsNull(cache.Get("Key2"));
         }
     }
 }
