78ed4855abe8158245380796c8ddc31f76cb9d6d
knownchen <knownchen@163.com>
2018-04-29 11:05:22 +0800

    Update Code Rule

---
+++ Known/Data/ColumnInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using Known.Extensions;
+using Known.Mapping;
+
+namespace Known.Data
+{
+    /// <summary>
+    /// 数据库栏位信息。
+    /// </summary>
+    public class ColumnInfo
+    {
+        /// <summary>
+        /// 取得或设置是否是主键。
+        /// </summary>
+        public bool IsKey { get; set; }
+
+        /// <summary>
+        /// 取得或设置栏位名。
+        /// </summary>
+        public string ColumnName { get; set; }
+
+        /// <summary>
+        /// 取得或设置栏位对应的实体属性。
+        /// </summary>
+        public PropertyInfo Property { get; set; }
+
+        /// <summary>
+        /// 获取实体属性映射的数据库栏位名。
+        /// </summary>
+        /// <param name="property">实体属性。</param>
+        /// <returns></returns>
+        public static string GetColumnName(PropertyInfo property)
+        {
+            var attr = property.GetAttribute<ColumnAttribute>();
+            if (attr != null)
+                return attr.ColumnName;
+
+            if (property.PropertyType.IsSubclassOf(typeof(EntityBase)))
+                return property.Name + "Id";
+
+            return property.Name;
+        }
+    }
+}
--- Known/Data/CommandCache.cs
+++ Known/Data/CommandCache.cs
@@ -10,44 +10,6 @@ using Known.Mapping;
 namespace Known.Data
 {
     /// <summary>
-    /// 数据库栏位信息。
-    /// </summary>
-    class ColumnInfo
-    {
-        /// <summary>
-        /// 取得或设置是否是主键。
-        /// </summary>
-        public bool IsKey { get; set; }
-
-        /// <summary>
-        /// 取得或设置栏位名。
-        /// </summary>
-        public string ColumnName { get; set; }
-
-        /// <summary>
-        /// 取得或设置栏位对应的实体属性。
-        /// </summary>
-        public PropertyInfo Property { get; set; }
-
-        /// <summary>
-        /// 获取实体属性映射的数据库栏位名。
-        /// </summary>
-        /// <param name="property">实体属性。</param>
-        /// <returns></returns>
-        public static string GetColumnName(PropertyInfo property)
-        {
-            var attr = property.GetAttribute<ColumnAttribute>();
-            if (attr != null)
-                return attr.ColumnName;
-
-            if (property.PropertyType.IsSubclassOf(typeof(EntityBase)))
-                return property.Name + "Id";
-
-            return property.Name;
-        }
-    }
-
-    /// <summary>
     /// 数据库命令缓存类。
     /// </summary>
     public class CommandCache
