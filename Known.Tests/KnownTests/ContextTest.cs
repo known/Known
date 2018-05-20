f64c9face7cb5ba29bfe61c7f3b8e1f3997a2837
knownchen <knownchen@163.com>
2018-05-20 20:39:22 +0800

    Add ContextTest

---
+++ Known.Tests/KnownTests/ContextTest.cs
@@ -0,0 +1,46 @@
+﻿using Known.Log;
+
+namespace Known.Tests.KnownTests
+{
+    public class ContextTest
+    {
+        public static void TestConstructor1()
+        {
+            var context = new Context(new ConsoleLogger());
+            Assert.IsNotNull(context.Logger);
+            Assert.IsNull(context.Database);
+            Assert.IsNull(context.UserName);
+            Assert.IsNull(context.Param);
+        }
+
+        public static void TestConstructor2()
+        {
+            var context = new Context(Config.GetDatabase(), new ConsoleLogger());
+            Assert.IsNotNull(context.Logger);
+            Assert.IsNotNull(context.Database);
+            Assert.IsNull(context.UserName);
+            Assert.IsNull(context.Param);
+        }
+
+        public static void TestConstructor3()
+        {
+            var context = new Context(Config.GetDatabase(), new ConsoleLogger(), "Known");
+            Assert.IsNotNull(context.Logger);
+            Assert.IsNotNull(context.Database);
+            Assert.IsNotNull(context.UserName);
+            Assert.IsNull(context.Param);
+            Assert.IsEqual(context.UserName, "Known");
+        }
+
+        public static void TestDynamicParam()
+        {
+            var context = new Context(new ConsoleLogger())
+            {
+                Param = new { Id = 1, Name = "Known" }
+            };
+            Assert.IsNotNull(context.Param);
+            Assert.IsEqual(context.Param.Id, 1);
+            Assert.IsEqual(context.Param.Name, "Known");
+        }
+    }
+}
