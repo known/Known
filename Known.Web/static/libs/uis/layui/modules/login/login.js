6203d80f0be04c7281817bfe45ad5b4f0f575525
knownchen <knownchen@163.com>
2018-06-22 09:47:31 +0800

    min file

--- Known.Web/static/libs/uis/layui/modules/login/login.js
+++
@@ -1,8 +0,0 @@
-layui.use('form', function () {
-    var form = layui.form;
-
-    form.on("submit(login)", function (data) {
-        window.location.href = '/';
-        return false;
-    });
-});
---
+++ Known.Web/static/modules/frame/index.js
@@ -0,0 +1,49 @@
+﻿function activeTab(item) {
+    var tabs = mini.get("mainTabs");
+    var tab = tabs.getTab(item.id);
+    if (!tab) {
+        tab = { name: item.id, title: item.text, url: item.url, iconCls: item.iconCls, showCloseButton: true };
+        tab = tabs.addTab(tab);
+    }
+    tabs.activeTab(tab);
+}
+
+$(function () {
+
+    //menu
+    var menu = new Menu("#mainMenu", {
+        itemclick: function (item) {
+            if (!item.children) {
+                activeTab(item);
+            }
+        }
+    });
+
+    $(".sidebar").mCustomScrollbar({ autoHideScrollbar: true });
+
+    new MenuTip(menu);
+
+    $.ajax({
+        url: "/menu.json",
+        success: function (text) {
+            var data = mini.decode(text);
+            menu.loadData(data);
+        }
+    })
+
+    //toggle
+    $("#toggle, .sidebar-toggle").click(function () {
+        $('body').toggleClass('compact');
+        mini.layout();
+    });
+
+    //dropdown
+    $(".dropdown-toggle").click(function (event) {
+        $(this).parent().addClass("open");
+        return false;
+    });
+
+    $(document).click(function (event) {
+        $(".dropdown").removeClass("open");
+    });
+});
