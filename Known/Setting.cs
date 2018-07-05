d1b5421a321068f7db5b2d14026c348470ff8c15
knownchen <knownchen@163.com>
2018-07-05 20:46:55 +0800

    Adding Setting class

---
+++ Known/Setting.cs
@@ -0,0 +1,25 @@
+﻿namespace Known
+{
+    /// <summary>
+    /// 应用程序设定。
+    /// </summary>
+    public class Setting
+    {
+        private static readonly Setting instance = new Setting();
+
+        private Setting() { }
+
+        /// <summary>
+        /// 取得应用程序设定实例。
+        /// </summary>
+        public Setting Instance
+        {
+            get { return instance; }
+        }
+
+        /// <summary>
+        /// 取得是否启用数据修改前日志。
+        /// </summary>
+        public bool IsDbAudit { get; }
+    }
+}
