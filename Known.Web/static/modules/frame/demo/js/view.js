d1a3baa4ee2950a23cc088416bebf312b79f1783
knownchen <knownchen@163.com>
2018-07-15 19:15:42 +0800

    Update demo view

--- Known.Web/static/modules/frame/demo/js/view.js
+++ Known.Web/static/modules/frame/demo/js/view.js
@@ -2,10 +2,15 @@
     element: {},
     show: function () {
         this.element = {
-
+            tree: mini.get('leftTree')
         };
+        this.element.tree.on('nodeselect', DemoView.onTreeNodeSelect);
     },
     onTreeNodeSelect: function (e) {
-
+        $('#page').loadHtml('/frame/partial', {
+            name: e.node.view
+        }, function () {
+            mini.parse();
+        });
     }
 };
