008a95dcee782fb8d0f47a5a61402638fe20c641
knownchen <knownchen@163.com>
2018-07-15 18:09:41 +0800

    Update file structure

---
+++ Known.Web/static/modules/index/js/navbar.js
@@ -0,0 +1,14 @@
+﻿var Navbar = {
+    demo: function () {
+        var tab = MainTabs.active({
+            id: 'demo', iconCls: 'fa-puzzle-piece',
+            text: '开发示例', url: '/view/demoview'
+        });
+    },
+    todo: function () {
+        MainTabs.active({
+            id: 'todo', iconCls: 'fa-paper-plane',
+            text: '代办事项', url: '/home/todo'
+        });
+    }
+};
---
+++ Known.Web/static/modules/index/js/userMenu.js
@@ -0,0 +1,15 @@
+﻿var UserMenu = {
+    info: function () {
+        Ajax.getJson('/api/user/getuserinfo', function (data) {
+        });
+    },
+    updPwd: function () {
+    },
+    logout: function () {
+        Message.confirm('确定要退出系统？', function () {
+            Ajax.postText('/user/signout', function () {
+                location = location;
+            });
+        });
+    }
+};
