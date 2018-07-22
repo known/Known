8337f8786c6865477b7ec43a37055a63bcd2d9c0
knownchen <knownchen@163.com>
2018-07-22 18:35:49 +0800

    Remove api log

--- Known.Web/Controllers/ApiController.cs
+++ Known.Web/Controllers/ApiController.cs
@@ -9,51 +9,37 @@ namespace Known.Web.Controllers
         [HttpPost]
         public ActionResult Query(string url, string query, string isLoad)
         {
-            log.Info($"get: {url} query: {query}");
             var param = FromJson(query);
             param.IsLoad = isLoad == "1";
             param.PageIndex = Request.Get<int>("pageIndex");
             param.PageSize = Request.Get<int>("pageSize");
             param.SortField = Request.Get<string>("sortField");
             param.SortOrder = Request.Get<string>("sortOrder");
+
             var result = Api.Post<ApiResult>(url, param);
             if (result.Status == 1)
-            {
-                log.Info($"error: {result.Message}");
                 return ErrorResult(result.Message);
-            }
 
-            log.Info($"success: {ToJson(result.Data)}");
             return JsonResult(result.Data);
         }
 
         [HttpGet]
         public ActionResult Get(string url, string param = null)
         {
-            log.Info($"get: {url} param: {param}");
             var result = Api.Get<ApiResult>(url, FromJson(param));
             if (result.Status == 1)
-            {
-                log.Info($"error: {result.Message}");
                 return ErrorResult(result.Message);
-            }
 
-            log.Info($"success: {ToJson(result.Data)}");
             return SuccessResult("", result.Data);
         }
 
         [HttpPost]
         public ActionResult Post(string url, string param = null)
         {
-            log.Info($"post: {url} param: {param}");
             var result = Api.Post<ApiResult>(url, FromJson(param));
             if (result.Status == 1)
-            {
-                log.Info($"error: {result.Message}");
                 return ErrorResult(result.Message);
-            }
 
-            log.Info($"success: {ToJson(result.Data)}");
             return SuccessResult("", result.Data);
         }
 
