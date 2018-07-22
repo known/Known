786a879e214a93ab6f4c0de23a00625b1805503b
knownchen <knownchen@163.com>
2018-07-22 08:03:51 +0800

    Adding System Module

---
+++ Known.Web/Controllers/SystemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Known.Web.Controllers
+{
+    public class SystemController : AuthorizeController
+    {
+        public ActionResult ModuleView()
+        {
+            return View();
+        }
+
+        public ActionResult RoleView()
+        {
+            return View();
+        }
+
+        public ActionResult UserView()
+        {
+            return View();
+        }
+    }
+}
