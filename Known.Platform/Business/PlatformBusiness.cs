9101f5defd076fd1e9bc91de7498cbcd870d5247
known <knownchen@163.com>
2018-08-05 16:37:33 +0800

    Adding DevTool

--- Known.Platform/Business/PlatformBusiness.cs
+++ Known.Platform/Business/PlatformBusiness.cs
@@ -10,15 +10,15 @@ namespace Known.Platform.Business
 
         public Module GetModule(string id)
         {
-            if (id == "demo")
+            if (id == "devTool")
             {
                 return new Module
                 {
-                    Id = "demo",
-                    Code = "Demo",
-                    Name = "开发示例",
+                    Id = "devTool",
+                    Code = "DevTool",
+                    Name = "开发工具",
                     ViewType = ViewType.SplitPageView,
-                    Extension = new { LeftPartialName = "Demo/DemoMenu" }.ToJson()
+                    Extension = new { LeftPartialName = "DevTool/LeftMenu" }.ToJson()
                 };
             }
 
