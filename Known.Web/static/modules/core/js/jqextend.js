c793f4011f4dd2f617d1b152f51510ec53a22b88
knownchen <knownchen@163.com>
2018-11-17 09:33:17 +0800

    Adding cache for page html

--- Known.Web/static/modules/core/js/jqextend.js
+++ Known.Web/static/modules/core/js/jqextend.js
@@ -1,14 +1,26 @@
-﻿$.fn.extend({
+﻿var cachedPages = [];
+
+$.fn.extend({
 
     loadHtml: function (url, param, callback) {
-        var _this = $(this).html('加载中....');
-        Ajax.getText(url, param, function (result) {
-            if (!$.isPlainObject(result)) {
-                _this.html(result);
-                mini.parse();
-                callback && callback();
-            }
-        });
+        var _this = $(this), pageUrl = url + JSON.stringify(param);
+        var page = cachedPages.find(p => p.url === pageUrl);
+
+        if (!page) {
+            _this.html('加载中....');
+            Ajax.getText(url, param, function (result) {
+                if (!$.isPlainObject(result)) {
+                    cachedPages.push({ url: pageUrl, html: result });
+                    _this.html(result);
+                    mini.parse();
+                    callback && callback();
+                }
+            });
+        } else {
+            _this.html(page.html);
+            mini.parse();
+            callback && callback();
+        }
     }
 
 });
