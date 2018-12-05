697a5df24d5495fcde2cad5db9701aa913ce04ff
knownchen <knownchen@163.com>
2018-12-05 22:16:34 +0800

    corejs

--- Known.Web/static/modules/core/js/core.js
+++ Known.Web/static/modules/core/js/core.js
@@ -5,3 +5,31 @@
     Child.prototype.constructor = Child;
     Child.uber = Parent.prototype;
 }
+
+function object(o) {
+    function F() { }
+    F.prototype = o;
+    return new F();
+}
+
+function extendCopy(p) {
+    var c = {};
+    for (var i in p) {
+        c[i] = p[i];
+    }
+    c.uber = p;
+    return c;
+}
+
+function deepCopy(p, c) {
+    var c = c || {};
+    for (var i in p) {
+        if (typeof p[i] === 'object') {
+            c[i] = (p[i].constructor === Array) ? [] : {};
+            deepCopy(p[i], c[i]);
+        } else {
+            c[i] = p[i];
+        }
+    }
+    return c;
+}
--- Known.Web/static/modules/frame/index.js
+++ Known.Web/static/modules/frame/index.js
@@ -1,7 +1,35 @@
-﻿function GridBase() {
+﻿var GridBase = {
 
-}
+    option: null,
+    grid: null,
 
-function FormBase() {
+    add: function () {
+        this._showForm({
+            Id: '',
+            ParentId: this.option.pid,
+            Sort: this.grid.getData().length + 1,
+            Enabled: 'Y'
+        });
+    },
 
-}
+    edit: function () {
+        var _this = this;
+        this.grid.checkSelect(function (row) {
+            _this._showForm(row);
+        });
+    },
+
+    remove: function () {
+        this.grid.deleteRows(function (rows) {
+        });
+    },
+
+    close: function () {
+        window.CloseOwnerWindow();
+    }
+
+};
+
+var FormBase = {
+
+};
