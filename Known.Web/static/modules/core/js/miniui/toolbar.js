a86034327d53e8eac0686b88a317d9ea54b5ce14
knownchen <knownchen@163.com>
2018-12-06 21:53:05 +0800

    Fixed工具条按钮事件重复问题

--- Known.Web/static/modules/core/js/miniui/toolbar.js
+++ Known.Web/static/modules/core/js/miniui/toolbar.js
@@ -2,7 +2,8 @@
 var Toolbar = function (tbId, handler) {
     this.tbId = tbId;
 
-    var buttons = $('#' + tbId).find('.button,.mini-button,.mini-menuitem');
+    var buttons = $('#' + tbId).find('.button,.mini-button,.mini-menuitem')
+                               .unbind('click');
     for (var i = 0; i < buttons.length; i++) {
         var button = $(buttons[i]),
             buttonId = button[0].id;
