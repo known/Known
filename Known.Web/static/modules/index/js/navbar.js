887c46eaa9c760eea83cf05a3c7c3b5b87852d38
knownchen <knownchen@163.com>
2018-12-21 21:25:37 +0800

    Updating navbar action

--- Known.Web/static/modules/index/js/navbar.js
+++ Known.Web/static/modules/index/js/navbar.js
@@ -3,14 +3,14 @@
     devTool: function () {
         var tab = MainTabs.active({
             id: 'devTool', iconCls: 'fa-puzzle-piece',
-            text: '开发工具', url: '/Frame?mid=devTool'
+            text: '开发工具', url: '/frame?mid=devTool'
         });
     },
 
     todo: function () {
         MainTabs.active({
             id: 'todo', iconCls: 'fa-paper-plane',
-            text: '代办事项', url: '/Frame?mid=todo'
+            text: '代办事项', url: '/frame?mid=todo'
         });
     },
 
@@ -31,7 +31,7 @@
 
     logout: function () {
         Message.confirm('确定要退出系统？', function () {
-            Ajax.postText('/User/SignOut', function () {
+            Ajax.postText('/logout', function () {
                 location = location;
             });
         });
