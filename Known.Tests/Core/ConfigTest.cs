15ef9bb49d093e544eaae9674a670ce1383af116
known <knownchen@163.com>
2019-01-02 20:29:36 +0800

    Updated Config Test

--- Known.Tests/Core/ConfigTest.cs
+++ Known.Tests/Core/ConfigTest.cs
@@ -7,6 +7,8 @@
             Assert.IsEqual(Config.AppSetting("test"), "测试");
             Assert.IsEqual(Config.AppSetting<int>("vint"), 1);
             Assert.IsEqual(Config.AppSetting<bool>("vbool"), true);
+            Assert.IsEqual(Config.AppSetting("none", "test"), "test");
+            Assert.IsEqual(Config.AppSetting<int>("int", 1), 1);
         }
     }
 }
