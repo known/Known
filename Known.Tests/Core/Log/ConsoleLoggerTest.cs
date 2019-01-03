30d91ab836a65e14bd5223ed97caeb10a770aa43
known <knownchen@163.com>
2019-01-03 20:38:46 +0800

    Added Log Test

---
+++ Known.Tests/Core/Log/ConsoleLoggerTest.cs
@@ -0,0 +1,19 @@
+﻿using Known.Log;
+
+namespace Known.Tests.Core.Log
+{
+    public class ConsoleLoggerTest
+    {
+        public static void TestConsoleLogger()
+        {
+            var logger = new ConsoleLogger();
+            for (int i = 0; i < 3; i++)
+            {
+                logger.Trace("测试Trace{0}", i);
+            }
+            logger.Info("测试结束");
+            logger.Info("这是Trace信息：{0}", logger.TraceInfo);
+            logger.Error("发生未知错误");
+        }
+    }
+}
