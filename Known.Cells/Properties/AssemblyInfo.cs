64b0c95e931450d1a88b5038072f953bd80959d3
known <knownchen@163.com>
2019-01-11 11:20:12 +0800

    Updated AssemblyInfo

--- Known.Cells/Properties/AssemblyInfo.cs
+++ Known.Cells/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("64c82219-f397-4b5a-b837-7a8ad4b7b721")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Known.Tests/Properties/AssemblyInfo.cs
+++ Known.Tests/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("93453e2e-b10e-4022-a052-825ef238b720")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
