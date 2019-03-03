f7d92a572521868051221f863fe6da0f10fd2eed
known <knownchen@163.com>
2019-03-03 14:41:32 +0800

    Updated Platform

---
+++ Known.WebMvc/Controllers/DevToolController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Known.WebMvc.Controllers
+{
+    public class DevToolController : AuthorizeController
+    {
+        public ActionResult DevToolView()
+        {
+            return View();
+        }
+    }
+}
