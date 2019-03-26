6c64424a92654dc66e4a8793dd9cd14ed5f78a02
known <knownchen@163.com>
2019-03-26 22:20:37 +0800

    Updated easyui defaults

--- Known.WebApi/static/js/index.js
+++ Known.WebApi/static/js/index.js
@@ -1,4 +1,11 @@
-﻿var Index = {
+﻿$.fn.panel.defaults.loadingMessage = '加载中...';
+$.fn.datagrid.defaults.loadMsg = '数据加载中，请稍等...';
+$.fn.datagrid.defaults.pageList = [10, 20, 30, 40, 50, 100, 200, 500];
+$.fn.pagination.defaults.displayMsg = '共 {total} 条数据';
+$.fn.pagination.defaults.beforePageText = '第';
+$.fn.pagination.defaults.afterPageText = '/{pages}页';
+
+var Index = {
 
     mainTabs: null,
 
