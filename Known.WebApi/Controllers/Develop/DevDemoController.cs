a27924db7bde094e8308e2902826e615649afaa7
known <knownchen@163.com>
2019-04-28 21:32:35 +0800

    update controller

---
+++ Known.WebApi/Controllers/Develop/DevDemoController.cs
@@ -0,0 +1,22 @@
+﻿using Known.Core.Services;
+using Known.Web;
+
+namespace Known.WebApi.Controllers.Develop
+{
+    public class DevDemoController : ApiControllerBase
+    {
+        private DevDemoService Service
+        {
+            get { return Container.Resolve<DevDemoService>(); }
+        }
+
+        #region DemoGrid
+        public ApiResult QueryUsers(PagingCriteria criteria)
+        {
+            //var criteria = GetPagingCriteria();
+            var result = Service.QueryUsers(criteria);
+            return ApiResult.ToPageData(result);
+        }
+        #endregion
+    }
+}
---
+++ Known.WebApi/Controllers/Develop/DevToolController.cs
@@ -0,0 +1,22 @@
+﻿using Known.Core.Services;
+using Known.Web;
+
+namespace Known.WebApi.Controllers.Develop
+{
+    public class DevToolController : ApiControllerBase
+    {
+        private DevToolService Service
+        {
+            get { return Container.Resolve<DevToolService>(); }
+        }
+
+        #region DevDatabase
+        public ApiResult QueryDatas(PagingCriteria criteria)
+        {
+            //var criteria = GetPagingCriteria();
+            var result = Service.QueryDatas(criteria);
+            return ApiResult.ToPageData(result);
+        }
+        #endregion
+    }
+}
