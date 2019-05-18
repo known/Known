0ddabad6f9ae77ac3add041114d4e6f737bc1cae
known <knownchen@163.com>
2019-05-18 20:12:10 +0800

    add role

---
+++ Known.WebApi/Controllers/System/RoleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Known.WebApi.Controllers.System
+{
+    public class RoleController : ApiControllerBase
+    {
+        #region RoleView
+        #endregion
+
+        #region RoleForm
+        #endregion
+    }
+}
