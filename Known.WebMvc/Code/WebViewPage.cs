f514722a207a9c720c16bbffd86aafe8a3c94fdb
known <knownchen@163.com>
2019-05-24 21:09:47 +0800

    Remove project

--- Known.WebMvc/Code/WebViewPage.cs
+++
@@ -1,9 +0,0 @@
-﻿using Known.Web;
-
-namespace Known.WebMvc
-{
-    public abstract class WebViewPage<T> : System.Web.Mvc.WebViewPage<T>
-    {
-        public ApiClient Api { get; }
-    }
-}
--- Known.WebMvc/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Known.WebMvc.Global" Language="C#" %>
