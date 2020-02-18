c219bcf52ce83fa7a182a6c2af8bd4ee7aec208d
known <knownchen@163.com>
2020-02-18 19:46:49 +0800

    update web

--- Known/PagingResult.cs
+++ Known/PagingResult.cs
@@ -10,10 +10,12 @@
         /// </summary>
         /// <param name="totalCount">查询结果总条数。</param>
         /// <param name="pageData">当前页数据集合。</param>
-        public PagingResult(int totalCount, object pageData)
+        /// <param name="summary">当前查询结果的摘要数据。</param>
+        public PagingResult(int totalCount, object pageData, object summary = null)
         {
             TotalCount = totalCount;
             PageData = pageData;
+            Summary = summary;
         }
 
         /// <summary>
@@ -25,5 +27,10 @@
         /// 取得当前页数据集合。
         /// </summary>
         public object PageData { get; }
+
+        /// <summary>
+        /// 取得当前查询结果的摘要数据。
+        /// </summary>
+        public object Summary { get; }
     }
 }
