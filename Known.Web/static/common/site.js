d112cb4f08b4d8e808e72b637f6cc3775d3ea036
KnownChen <knownchen@163.com>
2020-03-07 18:31:26 +0800

    Update Toolbar.js

--- Known.Web/static/common/site.js
+++ Known.Web/static/common/site.js
@@ -197,34 +197,6 @@ var Url = {
 
 };
 
-//---------------------------toolbar------------------------------------------//
-var Toolbar = {
-
-    bind: function (tbId, obj) {
-        for (var p in obj) {
-            bindButton(tbId, p, obj);
-        }
-
-        var top = !tbId.startsWith('tbForm');
-        var btnClose = $('#' + tbId + ' #close');
-        if (btnClose.length) {
-            btnClose.unbind('click').bind('click', function () {
-                Dialog.close(top);
-            });
-        }
-
-        function bindButton(tbId, name, obj) {
-            var btn = $('#' + tbId + ' #' + name);
-            if (btn.length) {
-                btn.unbind('click').bind('click', function () {
-                    obj[name].call(obj);
-                });
-            }
-        }
-    }
-
-};
-
 //---------------------------message------------------------------------------//
 var Message = {
 
@@ -265,6 +237,34 @@ var Message = {
 
     unmask: function () {
         $.messager.progress('close');
+    }
+
+};
+
+//---------------------------toolbar------------------------------------------//
+var Toolbar = {
+
+    bind: function (tbId, obj) {
+        for (var p in obj) {
+            bindButton(tbId, p, obj);
+        }
+
+        var top = !tbId.startsWith('tbForm');
+        var btnClose = $('#' + tbId + ' #close');
+        if (btnClose.length) {
+            btnClose.unbind('click').bind('click', function () {
+                Dialog.close(top);
+            });
+        }
+
+        function bindButton(tbId, name, obj) {
+            var btn = $('#' + tbId + ' #' + name);
+            if (btn.length) {
+                btn.unbind('click').bind('click', function () {
+                    obj[name].call(obj);
+                });
+            }
+        }
     }
 
 };
