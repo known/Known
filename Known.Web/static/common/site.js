da349e0fe51e1d3d4e3a03e26d25a160757c2aef
known <knownchen@163.com>
2020-03-08 13:08:05 +0800

    Update Dialog

--- Known.Web/static/common/site.js
+++ Known.Web/static/common/site.js
@@ -319,78 +319,18 @@ var Toolbar = {
 var Dialog = {
 
     show: function (option) {
-        if (option.id) {
-            var win = mini.get(option.id);
-            win.show();
-            option.callback && option.callback(win);
-            return win;
-        }
+        var options = $.extend({
+            width: '500px', height: 'auto', showType: 'fade', iconCls: 'fa-tv', cls: 'dialog',
+            collapsible: true, minimizable: true, maximizable: true, resizable: true
+        }, option);
 
-        var dialog = $('#dd').dialog({
-            title: 'My Dialog',
-            width: 400,
-            height: 200,
-            closed: false,
-            cache: false,
-            href: 'get_content.php',
-            modal: true
-        });
-
-        dialog.setTitle(option.title);
-        dialog.setIconCls(option.iconCls || 'fa-windows');
-        dialog.setWidth(option.width || 500);
-        dialog.setHeight(option.height || 300);
-        dialog.show();
-
-        if (option.max) {
-            dialog.max();
-        }
-
-        if (option.url) {
-            $('#dialog .mini-panel-body').loadHtml(
-                option.url,
-                option.param,
-                function () {
-                    mini.parse();
-                    option.callback && option.callback(dialog);
-                }
-            );
-        }
-
-        return dialog.dialog('dialog');
+        var dlg = $('#' + option.id).dialog(options);
+        dlg.dialog('open');
+        return dlg.dialog('dialog');
     },
 
-    open: function (option) {
-        var win = mini.open({
-            url: option.url,
-            showMaxButton: true,
-            allowResize: true,
-            title: option.title,
-            width: option.width,
-            height: option.height,
-            onload: function () {
-                if (option.callback) {
-                    var iframe = this.getIFrameEl();
-                    option.callback(iframe.contentWindow, 'load');
-                }
-            },
-            ondestroy: function (action) {
-                if (option.callback) {
-                    var iframe = this.getIFrameEl();
-                    option.callback(iframe.contentWindow, action);
-                }
-            }
-        });
-        option.max && win.max();
-        return win;
-    },
-
-    close: function (top = false) {
-        if (top) {
-            window.CloseOwnerWindow();
-        } else {
-            mini.get('dialog').hide();
-        }
+    close: function (id) {
+        $('#' + id).dialog('close');
     }
 
 };
