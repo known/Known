24108484bab82737c48cd0b5e26fe1cdbdfb1808
KnownChen <knownchen@163.com>
2020-04-04 17:11:48 +0800

    Update Web

--- Known.Web/static/js/index.js
+++ Known.Web/static/js/index.js
@@ -1,11 +1,70 @@
 layui.use(['layer', 'element'], function () {
-    var layer = layui.layer
-        , element = layui.element
-        , $ = layui.jquery;
+    var layer = layui.layer,
+        element = layui.element,
+        $ = layui.jquery;
 
     var url = {
-        GetMenus: '/Home/GetMenus?pid='
+        GetMenus: '/Home/GetMenus?pid=',
+        UserInfo: '/Home/UserInfo'
     };
+
+    var topRightAction = {
+        fullScreen: function () {
+            fullScreen();
+            $(this).data('type', 'exitScreen')
+                .html('<i class="layui-icon layui-icon-screen-restore"></i>');
+        },
+        exitScreen: function () {
+            exitScreen();
+            $(this).data('type', 'fullScreen')
+                .html('<i class="layui-icon layui-icon-screen-full"></i>');
+        },
+        userInfo: function () {
+            addTab({
+                id: 'userInfo',
+                text: $(this).text(),
+                icon: '',
+                url: url.UserInfo
+            });
+        },
+        logout: function () {
+            layer.confirm('确定要退出系统？', function (index) {
+                layer.close(index);
+            }); 
+        }
+    };
+
+    function fullScreen() {
+        var el = document.documentElement;
+        var rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
+
+        //typeof rfs != "undefined" && rfs
+        if (rfs) {
+            rfs.call(el);
+        } else if (typeof window.ActiveXObject !== "undefined") {
+            //for IE，这里其实就是模拟了按下键盘的F11，使浏览器全屏
+            var wscript = new ActiveXObject("WScript.Shell");
+            if (wscript !== null) {
+                wscript.SendKeys("{F11}");
+            }
+        }
+    }
+
+    function exitScreen() {
+        var el = document;
+        var cfs = el.cancelFullScreen || el.webkitCancelFullScreen || el.mozCancelFullScreen || el.exitFullScreen;
+
+        //typeof cfs != "undefined" && cfs
+        if (cfs) {
+            cfs.call(el);
+        } else if (typeof window.ActiveXObject !== "undefined") {
+            //for IE，这里和fullScreen相同，模拟按下F11键退出全屏
+            var wscript = new ActiveXObject("WScript.Shell");
+            if (wscript !== null) {
+                wscript.SendKeys("{F11}");
+            }
+        }
+    }
 
     function toTree(arr, rootId) {
         arr.forEach(function (element) {
@@ -58,6 +117,29 @@ layui.use(['layer', 'element'], function () {
         $(document).find(".layui-nav[lay-filter=" + obj + "]").html(html);
     }
 
+    function addTab(node) {
+        if (!node.url)
+            return;
+
+        var id = node.id;
+        var tab = $('.layui-tab-title li[lay-id="' + id + '"]');
+        if (!tab.length) {
+            var title = node.icon + ' ' + node.text;
+            var content = '<iframe src="' + node.url + '" frameborder="0" style="width:100%;height:100%;"></iframe>';
+            element.tabAdd('tabMenu', { id: id, title: title, content: content });
+        }
+        element.tabChange('tabMenu', id);
+    }
+
+    function init() {
+        initMenu('topMenu', '');
+
+        $('.top-right').on('click', function () {
+            var othis = $(this), type = othis.data('type');
+            topRightAction[type] ? topRightAction[type].call(this, othis) : '';
+        });
+    }
+
     element.on('nav(topMenu)', function (elem) {
         var pid = elem[0].id.replace('menu', '');
         initMenu('leftMenu', pid);
@@ -67,14 +149,12 @@ layui.use(['layer', 'element'], function () {
         var src = elem.data('url');
         if (src) {
             var a = elem[0];
-            var id = a.id.replace('menu', '');
-            var tab = $('.layui-tab-title li[lay-id="' + id + '"]');
-            if (!tab.length) {
-                var title = elem.find('i')[0].outerHTML + ' ' + a.text;
-                var content = '<iframe src="' + src + '" frameborder="0" style="width:100%;height:100%;"></iframe>';
-                element.tabAdd('tabMenu', { id: id, title: title, content: content });
-            }
-            element.tabChange('tabMenu', id);
+            addTab({
+                id: a.id.replace('menu', ''),
+                text: a.text,
+                icon: elem.find('i')[0].outerHTML,
+                url: src
+            });
         }
     });
 
@@ -84,7 +164,5 @@ layui.use(['layer', 'element'], function () {
         $('.layui-nav-child #menu' + id).parent().addClass('layui-this');
     });
 
-    initMenu('topMenu', '');
+    init();
 });
-
-
