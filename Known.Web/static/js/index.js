990b40eeae13171f58810fe4bc647ad92d8cc05a
KnownChen <knownchen@163.com>
2020-04-04 16:32:54 +0800

    Update Index

--- Known.Web/static/js/index.js
+++ Known.Web/static/js/index.js
@@ -3,6 +3,10 @@ layui.use(['layer', 'element'], function () {
         , element = layui.element
         , $ = layui.jquery;
 
+    var url = {
+        GetMenus: '/Home/GetMenus?pid='
+    };
+
     function toTree(arr, rootId) {
         arr.forEach(function (element) {
             var parentId = element.pid;
@@ -25,7 +29,7 @@ layui.use(['layer', 'element'], function () {
 
     function initMenu(obj, pid) {
         $.ajax({
-            url: '/Home/GetMenus?pid=' + pid, async: false,
+            url: url.GetMenus + pid, async: false,
             success: function (res) {
                 renderMenu(obj, res, pid);
                 if (pid === '') {
@@ -54,15 +58,6 @@ layui.use(['layer', 'element'], function () {
         $(document).find(".layui-nav[lay-filter=" + obj + "]").html(html);
     }
 
-    function init() {
-        initMenu('topMenu', '');
-    }
-
-    //$('.site-demo-active').on('click', function () {
-    //    var othis = $(this), type = othis.data('type');
-    //    active[type] ? active[type].call(this, othis) : '';
-    //});
-
     element.on('nav(topMenu)', function (elem) {
         var pid = elem[0].id.replace('menu', '');
         initMenu('leftMenu', pid);
@@ -73,23 +68,23 @@ layui.use(['layer', 'element'], function () {
         if (src) {
             var a = elem[0];
             var id = a.id.replace('menu', '');
-            var title = elem.find('i')[0].outerHTML + ' ' + a.text;
-            var content = '<iframe src="' + src + '" frameborder="0" style="width:100%;height:100%;"></iframe>';
-            element.tabAdd('tabMenu', { id: id, title: title, content: content });
+            var tab = $('.layui-tab-title li[lay-id="' + id + '"]');
+            if (!tab.length) {
+                var title = elem.find('i')[0].outerHTML + ' ' + a.text;
+                var content = '<iframe src="' + src + '" frameborder="0" style="width:100%;height:100%;"></iframe>';
+                element.tabAdd('tabMenu', { id: id, title: title, content: content });
+            }
             element.tabChange('tabMenu', id);
         }
     });
 
     element.on('tab(tabMenu)', function (elem) {
-        //console.log(elem);
-        //location.hash = 'test=' + $(this).attr('lay-id');
+        var id = $(this).attr('lay-id');
+        $('.layui-nav-child dd').removeClass('layui-this');
+        $('.layui-nav-child #menu' + id).parent().addClass('layui-this');
     });
 
-    //Hash地址的定位
-    //var layid = location.hash.replace(/^#test=/, '');
-    //element.tabChange('tabMenu', layid);
-
-    init();
+    initMenu('topMenu', '');
 });
 
 
