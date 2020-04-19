0dfffce26d17aed460824605eeba6fb5e0647a78
known <knownchen@163.com>
2020-04-19 10:04:33 +0800

    Add SysDictionary

---
+++ Known/Core/Entities/SysDictionary.cs
@@ -0,0 +1,13 @@
+﻿namespace Known.Core.Entities
+{
+    public class SysDictionary
+    {
+        public string Category { get; set; }
+        public string CategoryName { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Sort { get; set; }
+        public int Enabled { get; set; }
+        public string Note { get; set; }
+    }
+}
