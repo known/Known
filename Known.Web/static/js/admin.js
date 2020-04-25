3bb48fe01798262314642931ba29314898f02dd0
known <knownchen@163.com>
2020-04-25 18:33:46 +0800

    Update Index

--- Known.Web/static/js/admin.js
+++ Known.Web/static/js/admin.js
@@ -28,36 +28,33 @@ layui.define('index', function (exports) {
         return data;
     }
 
-    var admin = {
-        option: null,
-        init: function (option) {
-            this.option = option;
-            menuData = helper.list2Tree(option.data.menus, '');
-            var data = renderMenu('topMenu');
-            renderMenu('leftMenu', data[0].id);
-            option.callback && option.callback();
-        },
-        addTab: function (node) {
-            if (!node.url)
-                return;
-
-            var id = node.id;
-            var tab = $('.layui-tab-title li[lay-id="' + id + '"]');
-            if (!tab.length) {
-                var title = node.icon + ' <span>' + node.text + '</span>';
-                var content = '<iframe src="' + node.url + '" frameborder="0" class="layui-tab-iframe"></iframe>';
-                element.tabAdd('tabMenu', { id: id, title: title, content: content });
+    function fullScreen () {
+        var el = document.documentElement;
+        var rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
+        if (rfs) {
+            rfs.call(el);
+        } else if (typeof window.ActiveXObject !== 'undefined') {
+            //for IE，这里其实就是模拟了按下键盘的F11，使浏览器全屏
+            var wscript = new ActiveXObject('WScript.Shell');
+            if (wscript !== null) {
+                wscript.SendKeys('{F11}');
             }
-            element.tabChange('tabMenu', id);
-        },
-        getCurTab: function () {
-            var tab = $('.layui-tab-title .layui-this');
-            var id = tab.attr('lay-id');
-            var title = tab.children('span').text();
-            var module = this.option.data.menus.find(m => m.id === id);
-            return { id: id, title: title, module: module };
         }
-    };
+    }
+
+    function exitScreen () {
+        var el = document;
+        var cfs = el.cancelFullScreen || el.webkitCancelFullScreen || el.mozCancelFullScreen || el.exitFullScreen;
+        if (cfs) {
+            cfs.call(el);
+        } else if (typeof window.ActiveXObject !== 'undefined') {
+            //for IE，这里和fullScreen相同，模拟按下F11键退出全屏
+            var wscript = new ActiveXObject('WScript.Shell');
+            if (wscript !== null) {
+                wscript.SendKeys('{F11}');
+            }
+        }
+    }
 
     element.on('nav(topMenu)', function (elem) {
         var pid = elem[0].id.replace('menu', '');
@@ -82,6 +79,74 @@ layui.define('index', function (exports) {
         $('.layui-nav-child dd').removeClass('layui-this');
         $('.layui-nav-child #menu' + id).parent().addClass('layui-this');
     });
+
+    function initTopRight() {
+        $('.top-right').on('click', function () {
+            var othis = $(this), type = othis.data('type');
+            topRight[type] ? topRight[type].call(this, othis) : '';
+        });
+    }
+
+    var topRight = {
+        fullScreen: function () {
+            fullScreen();
+            $(this).data('type', 'exitScreen')
+                .html('<i class="layui-icon layui-icon-screen-restore"></i>');
+        },
+        exitScreen: function () {
+            exitScreen();
+            $(this).data('type', 'fullScreen')
+                .html('<i class="layui-icon layui-icon-screen-full"></i>');
+        },
+        userInfo: function () {
+            admin.addTab({
+                id: 'userInfo',
+                text: $(this).text(),
+                icon: $(this).find('i')[0].outerHTML,
+                url: admin.option.url.UserInfo
+            });
+        },
+        logout: function () {
+            helper.confirm('确定要退出系统？', function () {
+                $.post(admin.option.url.SignOut, function (result) {
+                    layer.msg(result.message);
+                    window.location = '/login';
+                });
+            });
+        }
+    };
+
+    var admin = {
+        option: null,
+        init: function (option) {
+            this.option = option;
+            menuData = helper.list2Tree(option.data.menus, '');
+            var data = renderMenu('topMenu');
+            renderMenu('leftMenu', data[0].id);
+            initTopRight();
+            option.callback && option.callback();
+        },
+        addTab: function (node) {
+            if (!node.url)
+                return;
+
+            var id = node.id;
+            var tab = $('.layui-tab-title li[lay-id="' + id + '"]');
+            if (!tab.length) {
+                var title = node.icon + ' <span>' + node.text + '</span>';
+                var content = '<iframe src="' + node.url + '" frameborder="0" class="layui-tab-iframe"></iframe>';
+                element.tabAdd('tabMenu', { id: id, title: title, content: content });
+            }
+            element.tabChange('tabMenu', id);
+        },
+        getCurTab: function () {
+            var tab = $('.layui-tab-title .layui-this');
+            var id = tab.attr('lay-id');
+            var title = tab.children('span').text();
+            var module = this.option.data.menus.find(m => m.id === id);
+            return { id: id, title: title, module: module };
+        }
+    };
 
     exports('admin', admin);
 });
--- Known.Web/static/js/helper.js
+++ Known.Web/static/js/helper.js
@@ -258,32 +258,6 @@ layui.define('index', function (exports) {
     }
 
     var helper = {
-        fullScreen: function () {
-            var el = document.documentElement;
-            var rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
-            if (rfs) {
-                rfs.call(el);
-            } else if (typeof window.ActiveXObject !== 'undefined') {
-                //for IE，这里其实就是模拟了按下键盘的F11，使浏览器全屏
-                var wscript = new ActiveXObject('WScript.Shell');
-                if (wscript !== null) {
-                    wscript.SendKeys('{F11}');
-                }
-            }
-        },
-        exitScreen: function () {
-            var el = document;
-            var cfs = el.cancelFullScreen || el.webkitCancelFullScreen || el.mozCancelFullScreen || el.exitFullScreen;
-            if (cfs) {
-                cfs.call(el);
-            } else if (typeof window.ActiveXObject !== 'undefined') {
-                //for IE，这里和fullScreen相同，模拟按下F11键退出全屏
-                var wscript = new ActiveXObject('WScript.Shell');
-                if (wscript !== null) {
-                    wscript.SendKeys('{F11}');
-                }
-            }
-        },
         list2Tree: function (data, pid) {
             return data.filter(father => {
                 let branchArr = data.filter(child => father['id'] === child['pid']);
