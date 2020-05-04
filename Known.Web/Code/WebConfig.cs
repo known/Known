10e44add7840bb8e195af038aff78d3d44675c40
known <knownchen@163.com>
2020-05-04 11:01:51 +0800

    Add Route namespace

--- Known.Web/Code/WebConfig.cs
+++ Known.Web/Code/WebConfig.cs
@@ -17,7 +17,8 @@ namespace Known.Web
             routes.MapRoute(
                 "Default",
                 "{controller}/{action}/{id}",
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "Known.Web.Controllers" }
             );
         }
     }
