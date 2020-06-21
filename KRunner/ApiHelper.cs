b537b22941da6d550340e147da5369a18471dae7
known <knownchen@163.com>
2020-06-21 21:35:25 +0800

    Add WebHelper

--- KRunner/ApiHelper.cs
+++ KRunner/ApiHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Net;
-using System.Text;
 using Known;
+using Known.Web;
 
 namespace KRunner
 {
@@ -36,38 +35,8 @@ namespace KRunner
             if (string.IsNullOrWhiteSpace(App.ApiUrl))
                 return;
 
-            var address = App.ApiUrl + path;
-            using (var client = new WebClient())
-            {
-                try
-                {
-                    client.Encoding = Encoding.UTF8;
-                    //if (!string.IsNullOrWhiteSpace(token))
-                    //    client.Headers.Add(HttpRequestHeader.Authorization, token);
-                    if (!string.IsNullOrWhiteSpace(App.ProxyUrl))
-                        client.Proxy = new WebProxy(App.ProxyUrl, false);
-
-                    var contentType = string.Empty;
-                    var postData = string.Empty;
-                    if (data.GetType() == typeof(string))
-                    {
-                        contentType = "application/x-www-form-urlencoded";
-                        postData = data.ToString();
-                    }
-                    else
-                    {
-                        contentType = "application/json";
-                        postData = Utils.ToJson(data);
-                    }
-
-                    client.Headers.Add(HttpRequestHeader.ContentType, contentType);
-                    client.UploadString(address, postData);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("{0}\n{1}\n{2}", address, Utils.ToJson(data), ex);
-                }
-            }
+            var url = App.ApiUrl + path;
+            WebHelper.Post(url, data, null, App.ProxyUrl);
         }
     }
 }
---
+++ Known/Web/WebHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace Known.Web
+{
+    public class WebHelper
+    {
+        public static void Post(string url, object data, string token = null, string proxyUrl = null)
+        {
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    client.Encoding = Encoding.UTF8;
+                    if (!string.IsNullOrWhiteSpace(token))
+                        client.Headers.Add(HttpRequestHeader.Authorization, token);
+                    if (!string.IsNullOrWhiteSpace(proxyUrl))
+                        client.Proxy = new WebProxy(proxyUrl, false);
+
+                    var contentType = string.Empty;
+                    var postData = string.Empty;
+                    if (data.GetType() == typeof(string))
+                    {
+                        contentType = "application/x-www-form-urlencoded";
+                        postData = data.ToString();
+                    }
+                    else
+                    {
+                        contentType = "application/json";
+                        postData = Utils.ToJson(data);
+                    }
+
+                    client.Headers.Add(HttpRequestHeader.ContentType, contentType);
+                    client.UploadString(url, postData);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(string.Format("{0}\n{1}\n{2}", url, Utils.ToJson(data), ex));
+                }
+            }
+        }
+    }
+}
