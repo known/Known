78bbc7a6046f4aa2ad13762282f6d9ddb44945ce
known <knownchen@163.com>
2020-06-23 11:17:58 +0800

    Add HttpServer

--- Known.Tests/Program.cs
+++ Known.Tests/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Known.Web;
 
 namespace Known.Tests
 {
@@ -10,10 +12,13 @@ namespace Known.Tests
     {
         static void Main(string[] args)
         {
-            var db = new Database();
-            var db1 = new Database("SQLite");
-            var db2 = new Database("Oracle");
-            var db3 = new Database("MySql");
+            //var db = new Database();
+            //var db1 = new Database("SQLite");
+            //var db2 = new Database("Oracle");
+            //var db3 = new Database("MySql");
+
+            var httpServer = new HttpServer();
+            httpServer.Start(9999);
 
             Console.WriteLine("按任意键结束！");
             Console.ReadKey();
