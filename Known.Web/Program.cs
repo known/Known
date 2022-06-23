e09aef27fa0a24fb66e76d93f1f78cd7244d8a20
KnownChen <knownchen@163.com>
2022-06-23 16:08:07 +0800

    改成SQLite数据库

--- Known.Web/Program.cs
+++ Known.Web/Program.cs
@@ -7,12 +7,13 @@
  * Change Logs:
  * Date           Author       Notes
  * 2020-08-20     KnownChen
+ * 2022-06-23     KnownChen    改成SQLite数据库
  * ------------------------------------------------------------------------------- */
 
 using Known.Web;
 
 KHost.Run(args, o =>
 {
-    o.DbFactories["MySqlConnector"] = typeof(MySqlConnector.MySqlConnectorFactory);
+    o.DbFactories["SQLite"] = typeof(Microsoft.Data.Sqlite.SqliteFactory);
     o.Modules.Add(typeof(Known.Dev.AppModule));
 });
