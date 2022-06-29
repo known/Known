e960441197d32b7cb8f619ad3739372455e8b786
KnownChen <knownchen@163.com>
2022-06-29 14:48:29 +0800

    添加ImgUtils类

--- Known/Core/HomeService.cs
+++ Known/Core/HomeService.cs
@@ -10,10 +10,6 @@
  * 2022-06-23     KnownChen    优化用户管理及登录
  * ------------------------------------------------------------------------------- */
 
-using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
 using System.Linq;
 using Known.Web;
 
@@ -86,64 +82,13 @@ namespace Known.Core
         [Anonymous]
         public ApiFile GetCaptcha()
         {
-            var bytes = CreateCaptcha(4, out string code);
+            var bytes = ImgUtils.CreateCaptcha(4, out string code);
             CaptchaCode = code;
             return new ApiFile
             {
                 ContentType = MimeTypes.ImageJpeg,
                 Bytes = bytes
             };
-        }
-
-        private static byte[] CreateCaptcha(int length, out string code)
-        {
-            code = string.Empty;
-            var textArray = "2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,U,V,W,X,Y,Z".Split(',');
-            for (var i = 0; i < length; i++)
-            {
-                var seekRand = new Random(i * (int)DateTime.Now.Ticks);
-                var index = seekRand.Next(textArray.Length);
-                code += textArray[index];
-            }
-
-            MemoryStream ms = null;
-            Color[] colors = { Color.Red, Color.DarkBlue, Color.Green, Color.Orange, Color.Brown, Color.DarkCyan, Color.Purple };
-            string[] fonts = { "Verdana", "Microsoft Sans Serif", "Comic Sans MS", "Arial" };
-
-            using (var img = new Bitmap(code.Length * 18, 32))
-            {
-                using (var g = Graphics.FromImage(img))
-                {
-                    g.Clear(Color.White);
-
-                    var random = new Random();
-                    //在随机位置画背景点  
-                    for (int i = 0; i < 250; i++)
-                    {
-                        int x = random.Next(img.Width);
-                        int y = random.Next(img.Height);
-                        g.DrawRectangle(new Pen(Color.LightGray, 0), x, y, 1, 1);
-                    }
-                    //验证码绘制在g中  
-                    for (int i = 0; i < code.Length; i++)
-                    {
-                        int cindex = random.Next(colors.Length);//随机颜色索引值  
-                        int findex = random.Next(fonts.Length);//随机字体索引值  
-                        Font f = new Font(fonts[findex], 15, FontStyle.Bold);//字体  
-                        Brush b = new SolidBrush(colors[cindex]);//颜色  
-                        int ii = 4;
-                        if ((i + 1) % 2 == 0)//控制验证码不在同一高度  
-                        {
-                            ii = 2;
-                        }
-                        g.DrawString(code.Substring(i, 1), f, b, 3 + (i * 16), ii);//绘制一个验证字符  
-                    }
-                    ms = new MemoryStream();//生成内存流对象  
-                    img.Save(ms, ImageFormat.Jpeg);//将此图像以Png图像文件的格式保存到流中
-                }
-            }
-
-            return ms.ToArray();
         }
         #endregion
 
