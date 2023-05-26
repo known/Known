5c834360a8576add14d2d653be3222b2c2eb1516
KnownChen <knownchen@163.com>
2023-05-26 21:32:25 +0800

    优化网站

--- WebSite/wwwroot/js/script.js
+++ WebSite/wwwroot/js/script.js
@@ -1,1 +1,10 @@
 prettyPrint();
+
+function showDocNav() {
+    var docNav = document.getElementById('docNav');
+    docNav.classList.add('show');
+}
+function hideDocNav() {
+    var docNav = document.getElementById('docNav');
+    docNav.classList.remove('show');
+}
