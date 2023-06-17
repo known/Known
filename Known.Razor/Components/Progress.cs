2e2cfb2bc1f0e02c9dc16aa884ff7bb8bb6ec0b6
KnownChen <knownchen@163.com>
2023-06-17 21:31:40 +0800

    增加进度条组件

---
+++ Known.Razor/Components/Progress.cs
@@ -0,0 +1,21 @@
+﻿namespace Known.Razor.Components;
+
+public class Progress : BaseComponent
+{
+    [Parameter] public int Width { get; set; }
+    [Parameter] public decimal Value { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Div("progress", attr =>
+        {
+            if (Width > 0)
+                attr.Style($"width:{Width}px;");
+            builder.Span("value", attr =>
+            {
+                attr.Style($"width:{Value * 100}%;");
+                builder.Text(Value.ToString("P0"));
+            });
+        });
+    }
+}
