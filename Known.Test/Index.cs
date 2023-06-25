1b7701a69985757d1e5034e8a4d9a4317c323eb9
Known <JPYQ@DESKTOP-3FI34JJ>
2023-06-25 17:25:07 +0800

    优化示例

--- Known.Test/Index.cs
+++ Known.Test/Index.cs
@@ -1,4 +1,15 @@
-﻿namespace Known.Test;
+﻿using Known.Razor.Pages;
+
+namespace Known.Test;
 
 [Route("/")]
-public class Index : Razor.Pages.Index { }
+public class Index : Razor.Pages.Index
+{
+    protected override void BuildLogin(RenderTreeBuilder builder)
+    {
+        builder.Component<Login>()
+               .Set(c => c.IsCaptcha, true)
+               .Set(c => c.OnLogin, OnLogin)
+               .Build();
+    }
+}
