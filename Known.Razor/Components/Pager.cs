6e1711e245cc353feaedfdf417e1c62cfb6e919e
KnownChen <knownchen@163.com>
2023-06-29 21:52:27 +0800

    优化分页组件

--- Known.Razor/Components/Pager.cs
+++ Known.Razor/Components/Pager.cs
@@ -35,6 +35,96 @@ class Pager : BaseComponent
 
     private void BuildPager(RenderTreeBuilder builder)
     {
+        // < 1 2 3 ... 5 > 第_1_页 确定 共100条 [20条/页]
+        builder.Ul(attr =>
+        {
+            BuildPrevious(builder);
+            BuildPageButton(builder, 1);
+            if (PageCount <= 5)
+            {
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                
+            }
+            BuildPageButton(builder, PageCount);
+            BuildNext(builder);
+            builder.Li(attr => BuildGoPage(builder));
+            builder.Li(attr => builder.Text($"共{TotalCount}条"));
+            builder.Li(attr => BuildPageSize(builder));
+        });
+    }
+
+    private void BuildPrevious(RenderTreeBuilder builder)
+    {
+        builder.Li("btn fa fa-chevron-left", attr =>
+        {
+            if (PageIndex > 1)
+                attr.OnClick(Callback(async e => await PageChanged(PageIndex--)));
+        });
+    }
+
+    private void BuildNext(RenderTreeBuilder builder)
+    {
+        builder.Li("btn fa fa-chevron-right", attr =>
+        {
+            if (PageIndex < PageCount)
+                attr.OnClick(Callback(async e => await PageChanged(PageIndex++)));
+        });
+    }
+
+    private void BuildPageButton(RenderTreeBuilder builder, int pageIndex)
+    {
+        builder.Li("btn", attr =>
+        {
+            attr.OnClick(Callback(async e => await PageChanged(pageIndex)));
+            builder.Text($"{pageIndex}");
+        });
+    }
+
+    private void BuildGoPage(RenderTreeBuilder builder)
+    {
+        builder.Text("第");
+        builder.Input(attr =>
+        {
+            attr.Type("text").Value($"{PageIndex}")
+                .OnChange(EventCallback.Factory.CreateBinder(this, value => PageIndex = value, PageIndex));
+        });
+        builder.Text("页");
+        builder.Button("确定", Callback(async e => await OnRefresh()));
+    }
+
+    private void BuildPageSize(RenderTreeBuilder builder)
+    {
+        var sizes = PagingCriteria.PageSizes;
+        builder.Select(attr =>
+        {
+            attr.OnChange(EventCallback.Factory.CreateBinder(this, value =>
+            {
+                PageIndex = 1;
+                PageSize = value;
+                OnRefresh();
+            }, PageSize));
+            foreach (var size in sizes)
+            {
+                builder.Option(attr =>
+                {
+                    attr.Value($"{size}").Selected(size == PageSize);
+                    builder.Text($"{size}条/页");
+                });
+            }
+        });
+    }
+
+    private Task PageChanged(int pageIndex)
+    {
+        PageIndex = pageIndex;
+        return OnRefresh();
+    }
+
+    private void BuildPager1(RenderTreeBuilder builder)
+    {
         var total = Language.PagerTotalText.Format(TotalCount);
         builder.Span("total", total);
 
@@ -60,28 +150,6 @@ class Pager : BaseComponent
             });
             BuildPageButton(builder, Language.PagerNext, "caret pn fa fa-caret-right");
             BuildPageButton(builder, Language.PagerLast, "fa fa-step-forward");
-        });
-    }
-
-    private void BuildPageSize(RenderTreeBuilder builder)
-    {
-        var sizes = PagingCriteria.PageSizes;
-        builder.Select(attr =>
-        {
-            attr.OnChange(EventCallback.Factory.CreateBinder(this, value =>
-            {
-                PageIndex = 1;
-                PageSize = value;
-                OnRefresh();
-            }, PageSize));
-            foreach (var size in sizes)
-            {
-                builder.Option(attr =>
-                {
-                    attr.Value($"{size}").Selected(size == PageSize);
-                    builder.Text($"{size}");
-                });
-            }
         });
     }
 
