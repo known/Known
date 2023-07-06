cf377ac2cedc3ef689af61a5d40fb1b80b49a437
KnownChen <knownchen@163.com>
2023-07-06 21:25:55 +0800

    新增User扩展方法发送站内信

---
+++ Known.Core/Extensions/UserExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace Known.Core.Extensions;
+
+public static class UserExtension
+{
+    public static void SendMessage(this UserInfo user, Database db, string toUser, string level, string subject, string content, string filePath = null, string bizId = null)
+    {
+        var model = new SysMessage
+        {
+            UserId = toUser,
+            Type = Constants.UMTypeReceive,
+            MsgBy = user.Name,
+            MsgLevel = level,
+            Subject = subject,
+            Content = content,
+            FilePath = filePath,
+            IsHtml = true,
+            Status = Constants.UMStatusUnread,
+            BizId = bizId
+        };
+        db.Save(model);
+    }
+}
