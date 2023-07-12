e70a181ab83810a2b6ae61df4bffa8307a420669
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-12 17:31:54 +0800

    新增Timeline组件

---
+++ Known.Razor/Components/Timeline.cs
@@ -0,0 +1,23 @@
+﻿namespace Known.Razor.Components;
+
+public class Timeline<T> : BaseComponent
+{
+    [Parameter] public string Style { get; set; }
+    [Parameter] public List<T> Items { get; set; }
+    [Parameter] public Action<RenderTreeBuilder, T> Template { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        var css = CssBuilder.Default("timeline").AddClass(Style).Build();
+        builder.Ul(css, attr =>
+        {
+            if (Items != null && Items.Count > 0)
+            {
+                foreach (var item in Items)
+                {
+                    builder.Li(attr => Template?.Invoke(builder, item));
+                }
+            }
+        });
+    }
+}
