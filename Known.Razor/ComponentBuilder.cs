3e8b01fe9bb81018bae6ea111162af80d9af668a
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-14 08:31:39 +0800

    优化代码

--- Known.Razor/ComponentBuilder.cs
+++ Known.Razor/ComponentBuilder.cs
@@ -10,6 +10,12 @@ public class ComponentBuilder<T> : AttributeBuilder<T> where T : BaseComponent
         return this;
     }
 
+    public ComponentBuilder<T> Name(string name)
+    {
+        Set(r => r.Name, name);
+        return this;
+    }
+
     public ComponentBuilder<T> ReadOnly(bool readOnly)
     {
         Set(r => r.ReadOnly, readOnly);
@@ -19,6 +25,12 @@ public class ComponentBuilder<T> : AttributeBuilder<T> where T : BaseComponent
     public ComponentBuilder<T> Enabled(bool enabled)
     {
         Set(r => r.Enabled, enabled);
+        return this;
+    }
+    
+    public ComponentBuilder<T> Visible(bool visible)
+    {
+        Set(r => r.Visible, visible);
         return this;
     }
 }
