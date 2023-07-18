7f507c8bb8f3d74fb5b7a4dcba512956313860f6
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-18 10:30:47 +0800

    优化示例

--- Known.Test/Pages/Samples/Homes/DemoHome4.cs
+++ Known.Test/Pages/Samples/Homes/DemoHome4.cs
@@ -1,5 +1,16 @@
-﻿namespace Known.Test.Pages.Samples.Homes;
+﻿using Known.Test.Pages.Samples.Forms;
+
+namespace Known.Test.Pages.Samples.Homes;
 
 class DemoHome4 : BaseComponent
 {
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Button("添加页签", Callback(OnAddTab), StyleType.Primary);
+    }
+
+    private void OnAddTab()
+    {
+        Context.Navigate<DemoForm1>("表单一", "fa fa-table");
+    }
 }
