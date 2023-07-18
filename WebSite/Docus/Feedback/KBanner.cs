498444d1cf330966e6e27eb0fd771f58df80125b
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-18 17:34:08 +0800

    优化网站

--- WebSite/Docus/Feedback/KBanner.cs
+++ WebSite/Docus/Feedback/KBanner.cs
@@ -6,24 +6,13 @@ class KBanner : BaseDocu
     {
         builder.BuildList(new string[]
         {
-            "内置后台主页默认布局为Header、Sider、Body组合",
-            "布局Header和Sider颜色可通过主题设置自定义",
-            "Body支持单页和多标签页",
-            "可自定义后台主页布局，重写Index页面的BuildAdmin方法"
+            "横幅可嵌入任何位置",
+            "支持默认、主要、成功、信息、警告、危险样式",
+            "默认手动点击图标关闭"
         });
     }
 
     protected override void BuildCodeDemo(RenderTreeBuilder builder)
     {
-        builder.BuildDemo<KLayout1>("默认布局", "class KLayout1 : Known.Razor.Pages.Index { }");
-    }
-
-    class KLayout1 : Known.Razor.Pages.Index
-    {
-        protected override Task OnInitializedAsync()
-        {
-            OnLogin(new Known.Models.UserInfo { Name = "管理员" });
-            return base.OnInitializedAsync();
-        }
     }
 }
