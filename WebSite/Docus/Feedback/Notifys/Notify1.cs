3202b4bb6710e08afa276078b00a1a55a0560d61
KnownChen <knownchen@163.com>
2023-07-24 21:23:03 +0800

    更新Notify组件文档

---
+++ WebSite/Docus/Feedback/Notifys/Notify1.cs
@@ -0,0 +1,22 @@
+﻿namespace WebSite.Docus.Feedback.Notifys;
+
+[Title("默认示例")]
+class Notify1 : BaseComponent
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Button("默认", Callback(OnDefault));
+        builder.Button("主要", Callback(OnPrimary), StyleType.Primary);
+        builder.Button("成功", Callback(OnSuccess), StyleType.Success);
+        builder.Button("信息", Callback(OnInfo), StyleType.Info);
+        builder.Button("警告", Callback(OnWarning), StyleType.Warning);
+        builder.Button("危险", Callback(OnDanger), StyleType.Danger);
+    }
+
+    private void OnDefault() => UI.Notify("这里是默认通知！", StyleType.Default);
+    private void OnPrimary() => UI.Notify("这里是主要通知！", StyleType.Primary);
+    private void OnSuccess() => UI.Notify("这里是成功通知！", StyleType.Success);
+    private void OnInfo() => UI.Notify("这里是信息通知！", StyleType.Info);
+    private void OnWarning() => UI.Notify("这里是警告通知！", StyleType.Warning);
+    private void OnDanger() => UI.Notify("这里是危险通知！", StyleType.Danger);
+}
---
+++ WebSite/Docus/Feedback/Notifys/Notify2.cs
@@ -0,0 +1,14 @@
+﻿namespace WebSite.Docus.Feedback.Notifys;
+
+[Title("自定义示例")]
+class Notify2 : BaseComponent
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Button("自定义内容", Callback(OnContent), StyleType.Primary);
+        builder.Button("自定义关闭时间", Callback(OnTime), StyleType.Primary);
+    }
+
+    private void OnContent() => UI.Notify("<h1>这里是自定义内容通知！</h1>", StyleType.Primary);
+    private void OnTime() => UI.Notify("这里是自定义关闭时间通知！<br/>10秒后自动关闭。", StyleType.Primary, 10000);
+}
