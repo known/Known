23e0dd82fadc8aeee33d530e346240e1e681b788
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-24 10:05:52 +0800

    更新Form示例

--- Known.Test/Pages/Samples/Forms/DemoForm1.cs
+++ Known.Test/Pages/Samples/Forms/DemoForm1.cs
@@ -1,5 +1,4 @@
 ﻿using Known.Test.Pages.Samples.DataList;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Known.Test.Pages.Samples.Forms;
 
@@ -24,9 +23,28 @@ class DemoForm1 : BaseForm
                 table.Field<Date>("月份", "Month").Set(f => f.DateType, DateType.Month).Build();
                 table.Field<Date>("日期时间", "DateTime").Set(f => f.DateType, DateType.DateTime).Build();
             });
-            table.Tr(attr => table.Field<RadioList>("单选", "RadioList").ColSpan(5).Set(f => f.Codes, Codes).Build());
-            table.Tr(attr => table.Field<CheckList>("多选", "CheckList", true).ColSpan(5).Set(f => f.Codes, Codes).Build());
-            table.Tr(attr => table.Field<Razor.Components.Fields.CheckBox>("状态", "Status").ColSpan(5).Set(f => f.Switch, true).Set(f => f.Text, "启用").Build());
+            table.Tr(attr =>
+            {
+                table.Field<Date>("星期", "Week").Set(f => f.DateType, DateType.Week).Build();
+                table.Field<Date>("时间", "Time").Set(f => f.DateType, DateType.Time).Build();
+                table.Field<Input>("颜色", "Color").Set(f => f.Type, InputType.Color).Build();
+            });
+            table.Tr(attr =>
+            {
+                table.Field<RadioList>("单选", "RadioList").ColSpan(3).Set(f => f.Codes, Codes).Build();
+                table.Field<Input>("邮箱", "Email").Set(f => f.Type, InputType.Email).Build();
+            });
+            table.Tr(attr =>
+            {
+                table.Field<CheckList>("多选", "CheckList", true).ColSpan(3).Set(f => f.Codes, Codes).Build();
+                table.Field<Input>("电话号码", "Tel").Set(f => f.Type, InputType.Tel).Build();
+            });
+            table.Tr(attr =>
+            {
+                table.Field<Razor.Components.Fields.CheckBox>("状态", "Status").Set(f => f.Switch, true).Set(f => f.Text, "启用").Build();
+                table.Field<Input>("搜索", "Search").Set(f => f.Type, InputType.Search).Build();
+                table.Field<Input>("URL", "Url").Set(f => f.Type, InputType.Url).Build();
+            });
             table.Tr(attr =>
             {
                 table.Th("", "选项");
@@ -70,6 +88,9 @@ class DemoForm1 : BaseForm
             Date = new DateTime(2020, 01, 01),
             Month = $"{DateTime.Now:yyyy-MM}",
             DateTime = DateTime.Now,
+            Week = "2023-W30",
+            Time = "11:59:00",
+            Color = "#009688",
             RadioList = "后羿",
             Status = true,
             CheckBox = true,
@@ -78,7 +99,11 @@ class DemoForm1 : BaseForm
             CheckList = "孙膑,妲己",
             Picker1 = "test1",
             Picker2 = "test1,test2",
-            TextArea = "Test Note"
+            TextArea = "Test Note",
+            Email = "test@test.com",
+            Tel = "1234567890",
+            Search = "test",
+            Url = "http://www.test.com"
         });
     }
 }
--- Known.Test/Pages/Samples/Forms/DemoForm5.cs
+++ Known.Test/Pages/Samples/Forms/DemoForm5.cs
@@ -4,25 +4,36 @@ namespace Known.Test.Pages.Samples.Forms;
 
 class DemoForm5 : BaseForm
 {
-    private DmBill model;
-
     protected override void OnInitialized()
     {
-        model = DmBill.LoadDefault();
-        Model = model;
     }
 
     protected override void BuildFields(FieldBuilder<DmBill> builder)
     {
         builder.Table(table =>
         {
-
+            table.ColGroup(11, 22, 11, 22, 11, 23);
+            table.Tr(attr =>
+            {
+                table.Field<Input>("颜色", "Color").Set(f => f.Type, InputType.Color).Build();
+                table.Field<Input>("邮箱", "Email").Set(f => f.Type, InputType.Email).Build();
+                table.Field<Input>("图片", "Image").Set(f => f.Type, InputType.Image).Build();
+            });
+            table.Tr(attr =>
+            {
+                table.Field<Input>("Slider", "Rrange").Set(f => f.Type, InputType.Rrange).Build();
+                table.Field<Input>("搜索", "Search").Set(f => f.Type, InputType.Search).Build();
+                table.Field<Input>("电话号码", "Tel").Set(f => f.Type, InputType.Tel).Build();
+            });
+            table.Tr(attr =>
+            {
+                table.Field<Input>("URL", "Url").Set(f => f.Type, InputType.Url).Build();
+            });
         });
     }
 
     public override void Save() => Submit(data =>
     {
-        model.FillModel(data);
-        formData = Utils.ToJson(model);
+        formData = Utils.ToJson(data);
     });
 }
