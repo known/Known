d2d48dab85aad618a504ac4e0ce498b24304f10f
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-26 17:16:10 +0800

    更新分页组件文档

--- WebSite/Docus/Nav/KPager.cs
+++ WebSite/Docus/Nav/KPager.cs
@@ -1,5 +1,20 @@
-﻿namespace WebSite.Docus.Nav;
+﻿using WebSite.Docus.Nav.Pagers;
+
+namespace WebSite.Docus.Nav;
 
 class KPager : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "分页组件自适应PC和移动端"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<Pager1>("1.默认示例", "block");
+        builder.BuildDemo<Pager2>("2.更新总条数示例", "block");
+    }
 }
---
+++ WebSite/Docus/Nav/Pagers/Pager2.cs
@@ -0,0 +1,33 @@
+﻿namespace WebSite.Docus.Nav.Pagers;
+
+class Pager2 : BaseComponent
+{
+    private Number? total;
+    private Pager? pager;
+    private PagingCriteria criteria = new(1);
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Div("control", attr =>
+        {
+            builder.Label("form-label", "条数：");
+            builder.Field<Number>("Total").Value("200").Set(f => f.Width, 100).Build(value => total = value);
+            builder.Button("设置", Callback(OnSetTotal), StyleType.Primary);
+        });
+
+        builder.Component<Pager>()
+               .Set(c => c.TotalCount, 120)
+               .Set(c => c.PageIndex, criteria.PageIndex)
+               .Set(c => c.PageSize, criteria.PageSize)
+               .Set(c => c.OnPageChanged, OnPageChanged)
+               .Build(value => pager = value);
+    }
+
+    private void OnSetTotal() => pager?.SetTotalCount(total.ValueAs<int>());
+
+    private Task OnPageChanged(PagingCriteria criteria)
+    {
+        this.criteria = criteria;
+        return Task.CompletedTask;
+    }
+}
