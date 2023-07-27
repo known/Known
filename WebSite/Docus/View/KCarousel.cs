be0540894df4256dcb0a2b9a0ff8f9d6d20decab
KnownChen <knownchen@163.com>
2023-07-27 20:46:02 +0800

    更新Carousel组件文档

--- WebSite/Docus/View/KCarousel.cs
+++ WebSite/Docus/View/KCarousel.cs
@@ -1,5 +1,19 @@
-﻿namespace WebSite.Docus.View;
+﻿using WebSite.Docus.View.Carousels;
+
+namespace WebSite.Docus.View;
 
 class KCarousel : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "用于轮流播放一组图片"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<Carousel1>("1.默认示例");
+    }
 }
