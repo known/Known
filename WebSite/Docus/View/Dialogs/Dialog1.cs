d294a8114f3f8e717b3d3192883bd4a98fa6078e
KnownChen <knownchen@163.com>
2023-07-27 21:17:36 +0800

    更新Dialog组件文档

---
+++ WebSite/Docus/View/Dialogs/Dialog1.cs
@@ -0,0 +1,23 @@
+﻿namespace WebSite.Docus.View.Dialogs;
+
+class Dialog1 : BaseComponent
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Button("提示对话框", Callback(OnAlert), StyleType.Primary);
+        builder.Button("确认对话框", Callback(OnConfirm), StyleType.Primary);
+    }
+
+    private void OnAlert()
+    {
+        UI.Alert("XXX逻辑校验失败！", size: new(300, 200));
+    }
+
+    private void OnConfirm()
+    {
+        UI.Confirm("确定要XXX操作？", () =>
+        {
+            UI.Toast("操作成功！", StyleType.Success);
+        });
+    }
+}
--- WebSite/Docus/View/KDialog.cs
+++ WebSite/Docus/View/KDialog.cs
@@ -1,5 +1,22 @@
-﻿namespace WebSite.Docus.View;
+﻿using WebSite.Docus.View.Dialogs;
+
+namespace WebSite.Docus.View;
 
 class KDialog : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "弹窗显示内容",
+            "弹出Alert、Confirm提示框",
+            "弹窗自定义组件内容",
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<Dialog1>("1.默认示例");
+        builder.BuildDemo<Dialog2>("2.组件示例");
+    }
 }
