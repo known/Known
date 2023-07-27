b7bb8015bf175e966d57f138c67ce8d01b2387b6
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 09:50:48 +0800

    更新Hidden组件文档

---
+++ WebSite/Docus/Input/Hiddens/Hidden1.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSite.Docus.Input.Hiddens;
+
+class Hidden1 : BaseComponent
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Field<Hidden>("Id");
+    }
+}
--- WebSite/Docus/Input/KHidden.cs
+++ WebSite/Docus/Input/KHidden.cs
@@ -1,5 +1,19 @@
-﻿namespace WebSite.Docus.Input;
+﻿using WebSite.Docus.Input.Hiddens;
+
+namespace WebSite.Docus.Input;
 
 class KHidden : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "隐藏字段组件"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<Hidden1>("1.默认示例");
+    }
 }
