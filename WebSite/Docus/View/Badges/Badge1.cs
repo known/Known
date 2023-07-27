9603e218ac9f216a462db26ed435d3b2a063f0ca
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 17:12:36 +0800

    更新Badge组件文档

---
+++ WebSite/Docus/View/Badges/Badge1.cs
@@ -0,0 +1,23 @@
+﻿namespace WebSite.Docus.View.Badges;
+
+class Badge1 : BaseComponent
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        BuildBadge(builder, StyleType.Default, "10");
+        BuildBadge(builder, StyleType.Primary, "10");
+        BuildBadge(builder, StyleType.Success, "10");
+        BuildBadge(builder, StyleType.Info, "10");
+        BuildBadge(builder, StyleType.Warning, "10");
+        BuildBadge(builder, StyleType.Danger, "10");
+    }
+
+    private static void BuildBadge(RenderTreeBuilder builder, StyleType style, string text)
+    {
+        builder.Div("demo-badge", attr =>
+        {
+            builder.Text("消息中心");
+            builder.Badge(style, text);
+        });
+    }
+}
--- WebSite/Docus/View/KBadge.cs
+++ WebSite/Docus/View/KBadge.cs
@@ -1,5 +1,19 @@
-﻿namespace WebSite.Docus.View;
+﻿using WebSite.Docus.View.Badges;
+
+namespace WebSite.Docus.View;
 
 class KBadge : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "支持默认、主要、成功、信息、警告、危险样式"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<Badge1>("1.默认示例");
+    }
 }
