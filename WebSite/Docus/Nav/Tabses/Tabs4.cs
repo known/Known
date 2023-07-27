d431bf028bacdffd1e698a04be1335ae3903fe14
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 16:39:43 +0800

    更新Tabs组件文档

---
+++ WebSite/Docus/Nav/Tabses/Tabs4.cs
@@ -0,0 +1,20 @@
+﻿namespace WebSite.Docus.Nav.Tabses;
+
+class Tabs4 : BaseComponent
+{
+    private readonly List<MenuItem> tabItems = new()
+    {
+        new MenuItem { Icon = "fa fa-file-o", Name = "Tab1" },
+        new MenuItem { Icon = "fa fa-file-o", Name = "Tab2" }
+    };
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Component<Tabs>()
+               .Set(c => c.Position, PositionType.Right)
+               .Set(c => c.CurItem, tabItems[0])
+               .Set(c => c.Items, tabItems)
+               .Set(c => c.Body, (b, m) => b.Span($"{m.Name} Content"))
+               .Build();
+    }
+}
