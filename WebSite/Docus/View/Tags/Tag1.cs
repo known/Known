7c1700db260d44000cb7a884f148363287d5c921
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 17:17:47 +0800

    更新Tag组件文档

---
+++ WebSite/Docus/View/Tags/Tag1.cs
@@ -0,0 +1,25 @@
+﻿namespace WebSite.Docus.View.Tags;
+
+class Tag1 : BaseComponent
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        BuildTag(builder, StyleType.Default, "测试");
+        BuildTag(builder, StyleType.Primary, "完成");
+        BuildTag(builder, StyleType.Success, "通过");
+        BuildTag(builder, StyleType.Info, "进行中");
+        BuildTag(builder, StyleType.Warning, "警告");
+        BuildTag(builder, StyleType.Danger, "失败");
+        BuildTag(builder, StyleType.Success, b => b.IconName("fa fa-user", "模板"));
+    }
+
+    private static void BuildTag(RenderTreeBuilder builder, StyleType style, string text)
+    {
+        builder.Div("demo-tag", attr => builder.Tag(style, text));
+    }
+
+    private static void BuildTag(RenderTreeBuilder builder, StyleType style, Action<RenderTreeBuilder> content)
+    {
+        builder.Div("demo-tag", attr => builder.Tag(style, content));
+    }
+}
