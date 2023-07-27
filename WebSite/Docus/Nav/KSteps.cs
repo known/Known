5363e50faffd15575d965f70cb9fd2a5e6194cc6
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 16:32:00 +0800

    更新Steps组件文档

--- WebSite/Docus/Nav/KSteps.cs
+++ WebSite/Docus/Nav/KSteps.cs
@@ -1,5 +1,19 @@
-﻿namespace WebSite.Docus.Nav;
+﻿using WebSite.Docus.Nav.Stepses;
+
+namespace WebSite.Docus.Nav;
 
 class KSteps : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "分步表单组件"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<Steps1>("1.默认示例");
+    }
 }
