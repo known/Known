bb4bf9a962580f8fab7daaaa5090c496501ab389
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 11:43:20 +0800

    更新SearchBox组件文档

--- WebSite/Docus/Inputs/KSearchBox.cs
+++ WebSite/Docus/Inputs/KSearchBox.cs
@@ -1,5 +1,19 @@
-﻿namespace WebSite.Docus.Inputs;
+﻿using WebSite.Docus.Inputs.SearchBoxs;
+
+namespace WebSite.Docus.Inputs;
 
 class KSearchBox : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "搜索框组件"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<SearchBox1>("1.默认示例");
+    }
 }
---
+++ WebSite/Docus/Inputs/SearchBoxs/SearchBox1.cs
@@ -0,0 +1,22 @@
+﻿namespace WebSite.Docus.Inputs.SearchBoxs;
+
+class SearchBox1 : BaseComponent
+{
+    private string? message;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Component<SearchBox>()
+               .Set(c => c.Required, true)
+               .Set(c => c.Placeholder, "请输入关键字")
+               .Set(c => c.OnSearh, OnSearh)
+               .Build();
+        builder.Div("tips", message);
+    }
+
+    private void OnSearh(string key)
+    {
+        message = key;
+        StateChanged();
+    }
+}
