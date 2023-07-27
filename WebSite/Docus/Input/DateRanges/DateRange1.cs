7704215b01948ce30fc0e4fba6516a484cb2080c
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 09:43:50 +0800

    更新DateRange组件文档

---
+++ WebSite/Docus/Input/DateRanges/DateRange1.cs
@@ -0,0 +1,13 @@
+﻿namespace WebSite.Docus.Input.DateRanges;
+
+class DateRange1 : BaseComponent
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Field<DateRange>("DateRange1").Build();
+        builder.Field<DateRange>("DateRange2")
+               .Set(f => f.Start, DateTime.Now)
+               .Set(f => f.End, DateTime.Now.AddMonths(1))
+               .Build();
+    }
+}
--- WebSite/Docus/Input/KDateRange.cs
+++ WebSite/Docus/Input/KDateRange.cs
@@ -1,5 +1,20 @@
-﻿namespace WebSite.Docus.Input;
+﻿using WebSite.Docus.Input.DateRanges;
+
+namespace WebSite.Docus.Input;
 
 class KDateRange : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "显示Date区间查询"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<DateRange1>("1.默认示例");
+        builder.BuildDemo<DateRange2>("2.控制示例");
+    }
 }
