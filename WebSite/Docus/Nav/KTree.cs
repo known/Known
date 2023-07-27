09822268009d662d3243cb6e1e9d2293912bd1a5
Known <JPYQ@DESKTOP-3FI34JJ>
2023-07-27 16:55:59 +0800

    更新Tree组件文档

--- WebSite/Docus/Nav/KTree.cs
+++ WebSite/Docus/Nav/KTree.cs
@@ -1,5 +1,20 @@
-﻿namespace WebSite.Docus.Nav;
+﻿using WebSite.Docus.Nav.Trees;
+
+namespace WebSite.Docus.Nav;
 
 class KTree : BaseDocu
 {
+    protected override void BuildOverview(RenderTreeBuilder builder)
+    {
+        builder.BuildList(new string[]
+        {
+            "树组件"
+        });
+    }
+
+    protected override void BuildCodeDemo(RenderTreeBuilder builder)
+    {
+        builder.BuildDemo<Tree1>("1.默认示例");
+        builder.BuildDemo<Tree2>("2.操作示例");
+    }
 }
---
+++ WebSite/Docus/Nav/Trees/Tree1.cs
@@ -0,0 +1,46 @@
+﻿namespace WebSite.Docus.Nav.Trees;
+
+class Tree1 : BaseComponent
+{
+    private string? message;
+    private readonly List<TreeItem<string>> data = new()
+    {
+        new TreeItem<string>
+        {
+            Value = "1",
+            Text = "辅助",
+            IsExpanded = true,
+            Children = new List<TreeItem<string>>
+            {
+                new TreeItem<string> { Value = "11", Text = "孙膑" },
+                new TreeItem<string> { Value = "12", Text = "庄周" }
+            }
+        },
+        new TreeItem<string>
+        {
+            Value = "2",
+            Text = "射手",
+            Children = new List<TreeItem<string>>
+            {
+                new TreeItem<string> { Value = "21", Text = "后羿" },
+                new TreeItem<string> { Value = "22", Text = "伽罗" }
+            }
+        }
+    };
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Component<Tree<string>>()
+               .Set(c => c.Data, data)
+               .Set(c => c.ReadOnly, ReadOnly)
+               .Set(c => c.OnItemClick, Callback<TreeItem<string>>(OnItemClick))
+               .Build();
+        builder.Div("tips", message);
+    }
+
+    private void OnItemClick(TreeItem<string> item)
+    {
+        message = $"{item.Value} - {item.Text}";
+        StateChanged();
+    }
+}
