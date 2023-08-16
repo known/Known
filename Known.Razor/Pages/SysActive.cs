0b2de48779e94eb22a23117defd37f7c4d947fc5
KnownChen <knownchen@163.com>
2023-08-16 21:24:58 +0800

    修复系统激活页面样式问题

--- Known.Razor/Pages/SysActive.cs
+++ Known.Razor/Pages/SysActive.cs
@@ -14,15 +14,16 @@ class SysActive : BaseComponent
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        builder.Component<Empty>()
-               .Set(c => c.Icon, "fa fa-shield")
-               .Set(c => c.Text, KRConfig.AuthStatus)
-               .Build();
         builder.Div("sys-active", attr =>
         {
+            builder.Component<Empty>()
+                   .Set(c => c.Icon, "fa fa-shield")
+                   .Set(c => c.Text, KRConfig.AuthStatus)
+                   .Build();
+
             builder.Field<Text>("产品ID：", "").Value(info?.ProductId).ReadOnly(true).Build();
             builder.Field<Text>("产品密钥：", "ProductKey").Value(info?.ProductKey).Build(value => productKey = value);
-            builder.Div("form-button", attr => builder.Button("授权", Callback(OnAuth)));
+            builder.Div("form-button", attr => builder.Button("授权", Callback(OnAuth), StyleType.Primary));
         });
     }
 
