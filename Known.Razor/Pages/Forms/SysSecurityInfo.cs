04abcbbc60fa1e88bfa5ce91925e151b3d44c7d6
Known <JPYQ@DESKTOP-3FI34JJ>
2023-09-09 09:41:52 +0800

    关于系统增加安全设置选项卡

---
+++ Known.Razor/Pages/Forms/SysSecurityInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace Known.Razor.Pages.Forms;
+
+class SysSecurityInfo : PageComponent
+{
+    private SystemInfo info;
+
+    protected override async Task InitPageAsync()
+    {
+        info = await Platform.System.GetSystemAsync();
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Div("ss-form ss-system", attr =>
+        {
+            builder.Field<Text>("默认密码：", "").Value(info?.UserDefaultPwd).ReadOnly(true)
+                   .Set(f => f.IsEdit, true)
+                   .Set(f => f.OnSave, async value =>
+                   {
+                       info.UserDefaultPwd = value;
+                       await Platform.System.SaveSystemAsync(info);
+                   })
+                   .Build();
+        });
+    }
+}
--- Known.Razor/Pages/SysSystem.cs
+++ Known.Razor/Pages/SysSystem.cs
@@ -1,77 +1,21 @@
-﻿namespace Known.Razor.Pages;
+﻿using Known.Razor.Pages.Forms;
+
+namespace Known.Razor.Pages;
 
 class SysSystem : PageComponent
 {
-    private SystemInfo info;
-    
-    protected override async Task InitPageAsync()
+    private readonly List<MenuItem> items = new()
     {
-        info = await Platform.System.GetSystemAsync();
-    }
+        new MenuItem("系统信息", "fa fa-info-circle", typeof(SysSystemInfo)),
+        new MenuItem("安全设置", "fa fa-shield", typeof(SysSecurityInfo))
+    };
 
-    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    protected override void BuildPage(RenderTreeBuilder builder)
     {
-        var user = CurrentUser;
-        var status = KRConfig.AuthStatus;
-        var style = string.IsNullOrWhiteSpace(status) ? "success" : "danger";
-        if (string.IsNullOrWhiteSpace(status))
-            status = "已授权";
-        builder.Div("ss-form ss-system", attr =>
-        {
-            var label = Config.IsPlatform ? "租户名称：" : "企业名称：";
-            builder.Field<Text>(label, "").Value($"{info?.CompNo}-{info?.CompName}").ReadOnly(true).Build();
-            builder.Field<Text>("系统名称：", "").Value(info?.AppName).ReadOnly(true)
-                   .Set(f => f.IsEdit, true)
-                   .Set(f => f.OnSave, async value =>
-                   {
-                       info.AppName = value;
-                       await Platform.System.SaveSystemAsync(info);
-                       PageAction.RefreshAppName?.Invoke(value);
-                   })
-                   .Build();
-            builder.Field<Text>("系统版本：", "").Value(Config.AppVersion).ReadOnly(true).Build();
-            builder.Field<Text>("软件版本：", "").Value(Config.SoftVersion).ReadOnly(true).Build();
-            builder.Field<Text>("框架版本：", "").Value(Config.FrameVersion).ReadOnly(true).Build();
-            builder.Field<Text>("默认密码：", "").Value(info?.UserDefaultPwd).ReadOnly(true)
-                   .Set(f => f.IsEdit, true)
-                   .Set(f => f.OnSave, async value =>
-                   {
-                        info.UserDefaultPwd = value;
-                        await Platform.System.SaveSystemAsync(info);
-                   })
-                   .Build();
-            if (!Config.IsPlatform)
-            {
-                builder.Field<Text>("产品ID：", "").Value(info?.ProductId).ReadOnly(true).Build();
-                builder.Field<Text>("产品密钥：", "ProductKey").Value(info?.ProductKey).ReadOnly(true)
-                       .Set(f => f.IsEdit, true)
-                       .Set(f => f.OnSave, async value =>
-                       {
-                           info.ProductKey = value;
-                           await Platform.System.SaveKeyAsync(info);
-                           StateChanged();
-                       })
-                       .Build();
-                builder.Field<Text>("授权信息：", "").InputTemplate(b => b.Span($"text bold {style}", status)).Build();
-            }
-            builder.Field<Text>("版权信息：", "").Style("ss-copyright").Value(info?.Copyright ?? KRConfig.Copyright).ReadOnly(true)
-                   .Set(f => f.IsEdit, KRConfig.IsEditCopyright && user.IsSystemAdmin())
-                   .Set(f => f.OnSave, async value =>
-                   {
-                       info.Copyright = value;
-                       await Platform.System.SaveSystemConfigAsync(info);
-                       StateChanged();
-                   })
-                   .Build();
-            builder.Field<TextArea>("软件许可：", "").Style("ss-terms").Value(info?.SoftTerms ?? KRConfig.SoftTerms).ReadOnly(true)
-                   .Set(f => f.IsEdit, KRConfig.IsEditCopyright && user.IsSystemAdmin())
-                   .Set(f => f.OnSave, async value =>
-                   {
-                       info.SoftTerms = value;
-                       await Platform.System.SaveSystemConfigAsync(info);
-                       StateChanged();
-                   })
-                   .Build();
-        });
+        builder.Component<Tabs>()
+               .Set(c => c.CurItem, items[0])
+               .Set(c => c.Items, items)
+               .Set(c => c.Body, (b, m) => b.DynamicComponent(m.ComType))
+               .Build();
     }
 }
