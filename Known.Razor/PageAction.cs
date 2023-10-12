20cc5f74cfcac067eaa350920f26d7f87dd86728
Known <JPYQ@DESKTOP-3FI34JJ>
2023-10-12 10:18:03 +0800

    优化PageAction访问级别

--- Known.Razor/PageAction.cs
+++ Known.Razor/PageAction.cs
@@ -4,9 +4,9 @@ public sealed class PageAction
 {
     private PageAction() { }
 
-    public static Action RefreshTheme { get; set; }
-    public static Action<string> RefreshAppName { get; set; }
-    public static Action<int> RefreshMessageCount { get; set; }
+    internal static Action RefreshTheme { get; set; }
+    internal static Action<string> RefreshAppName { get; set; }
+    internal static Action<int> RefreshMessageCount { get; set; }
 
     public static IPicker CreateUserPicker(string role) => new SysUserList(role);
 }
