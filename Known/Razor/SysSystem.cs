7ed27945b58f515b2ab6e36c69bd7d0abc6c5c63
KnownChen <knownchen@163.com>
2023-11-14 20:40:34 +0800

    初始化

---
+++ Known/Razor/SysSystem.cs
@@ -0,0 +1,14 @@
+﻿namespace Known.Razor;
+
+public class SysSystem : BasePage
+{
+    protected SystemInfo Model { get; private set; }
+
+    protected override async Task OnInitPageAsync()
+    {
+        await base.OnInitPageAsync();
+        Model = await Platform.System.GetSystemAsync();
+    }
+
+    protected Task<Result> SaveModelAsync() => Platform.System.SaveSystemAsync(Model);
+}
--- Known/SystemInfos.cs
+++ Known/SystemInfos.cs
@@ -1,28 +1,68 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Known;
 
 public class InstallInfo
 {
     public bool IsInstalled { get; set; }
-    [Required] public string CompNo { get; set; }
-    [Required] public string CompName { get; set; }
-    [Required] public string AppName { get; set; }
-    [Required] public string ProductId { get; set; }
-    [Required] public string ProductKey { get; set; }
-    [Required] public string UserName { get; set; }
-    [Required] public string Password { get; set; }
-    [Required] public string Password1 { get; set; }
+    
+    [DisplayName("企业编码")]
+    [Required(ErrorMessage = "企业编码不能为空！")]
+    public string CompNo { get; set; }
+    
+    [DisplayName("企业名称")]
+    [Required(ErrorMessage = "企业名称不能为空！")]
+    public string CompName { get; set; }
+
+    [DisplayName("系统名称")]
+    [Required(ErrorMessage = "系统名称不能为空！")]
+    public string AppName { get; set; }
+
+    [DisplayName("产品ID")]
+    [Required(ErrorMessage = "产品ID不能为空！")]
+    public string ProductId { get; set; }
+
+    [DisplayName("产品密钥")]
+    [Required(ErrorMessage = "产品密钥不能为空！")]
+    public string ProductKey { get; set; }
+
+    [DisplayName("管理员账号")]
+    [Required(ErrorMessage = "管理员账号不能为空！")]
+    public string UserName { get; set; }
+
+    [DisplayName("管理员密码")]
+    [Required(ErrorMessage = "管理员密码不能为空！")]
+    public string Password { get; set; }
+
+    [DisplayName("确认密码")]
+    [Required(ErrorMessage = "确认密码不能为空！")]
+    public string Password1 { get; set; }
 }
 
 public class SystemInfo
 {
+    [DisplayName("企业编码")]
     public string CompNo { get; set; }
+
+    [DisplayName("企业名称")]
     public string CompName { get; set; }
+
+    [DisplayName("系统名称")]
     public string AppName { get; set; }
+
+    [DisplayName("产品ID")]
     public string ProductId { get; set; }
+
+    [DisplayName("产品密钥")]
     public string ProductKey { get; set; }
+
+    [DisplayName("默认密码")]
     public string UserDefaultPwd { get; set; }
+
+    [DisplayName("版权信息")]
     public string Copyright { get; set; }
+
+    [DisplayName("软件许可")]
     public string SoftTerms { get; set; }
 }
