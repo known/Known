846b3fb55e21aa0f4e1ff300c5d1ceb48e5534d4
Known <JPYQ@DESKTOP-3FI34JJ>
2023-12-27 16:04:03 +0800

    初始化

--- Known.AntBlazor/Extension.cs
+++ Known.AntBlazor/Extension.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using AntDesign;
 using Known.Blazor;
+using Known.Extensions;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,40 +31,57 @@ public static class Extension
         var rules = new List<FormValidationRule>();
         if (column.Required && property.PropertyType != typeof(bool))
         {
-            //String,Number,Boolean,Regexp,Integer,Float,Array,Object,Enum,Date,Url,Email
-            var type = FormFieldType.String;
-            if (property.PropertyType.IsEnum)
-                type = FormFieldType.Enum;
-            else if (property.PropertyType == typeof(DateTime))
-                type = FormFieldType.Date;
-            else if (property.PropertyType.IsArray)
-                type = FormFieldType.Array;
-            else if (property.PropertyType == typeof(int) || property.PropertyType == typeof(uint))
-                type = FormFieldType.Integer;
-            else if (property.PropertyType == typeof(float) || property.PropertyType == typeof(double))
-                type = FormFieldType.Float;
-            rules.Add(new FormValidationRule()
-            {
-                Type = type,
-                Required = true,
-                Message = $"{column.Name}不能为空！"
-            });
+            rules.Add(GetFormRuleRequired(column));
         }
         else
         {
+            var min = property.MinLength();
+            if (min != null)
+                rules.Add(GetFormRuleMin(column, min.Value));
+
+            var max = property.MaxLength();
+            if (max != null)
+                rules.Add(GetFormRuleMax(column, max.Value));
+
             var regex = property?.GetCustomAttribute<RegexAttribute>();
             if (regex != null)
-            {
-                rules.Add(new FormValidationRule()
-                {
-                    Type = FormFieldType.Regexp,
-                    Pattern = regex.Pattern,
-                    Message = regex.Message
-                });
-            }
+                rules.Add(GetFormRuleRegex(regex));
         }
 
         return [.. rules];
+    }
+
+    private static FormValidationRule GetFormRuleRequired(ColumnInfo column)
+    {
+        //String,Number,Boolean,Regexp,Integer,Float,Array,Object,Enum,Date,Url,Email
+        var property = column.Property;
+        var type = FormFieldType.String;
+        if (property.PropertyType.IsEnum)
+            type = FormFieldType.Enum;
+        else if (property.PropertyType == typeof(DateTime))
+            type = FormFieldType.Date;
+        else if (property.PropertyType.IsArray)
+            type = FormFieldType.Array;
+        else if (property.PropertyType == typeof(int) || property.PropertyType == typeof(uint))
+            type = FormFieldType.Integer;
+        else if (property.PropertyType == typeof(float) || property.PropertyType == typeof(double))
+            type = FormFieldType.Float;
+        return new FormValidationRule { Type = type, Required = true, Message = $"{column.Name}不能为空！" };
+    }
+
+    private static FormValidationRule GetFormRuleMin(ColumnInfo column, int length)
+    {
+        return new FormValidationRule { Type = FormFieldType.String, Min = length, Message = $"{column.Name}至少{length}个字符！" };
+    }
+
+    private static FormValidationRule GetFormRuleMax(ColumnInfo column, int length)
+    {
+        return new FormValidationRule { Type = FormFieldType.String, Max = length, Message = $"{column.Name}不能超过{length}个字符！" };
+    }
+
+    private static FormValidationRule GetFormRuleRegex(RegexAttribute regex)
+    {
+        return new FormValidationRule { Type = FormFieldType.Regexp, Pattern = regex.Pattern, Message = regex.Message };
     }
 
     internal static RadioOption<string>[] ToRadioOptions(this List<CodeInfo> codes)
--- Known/Menus.cs
+++ Known/Menus.cs
@@ -135,6 +135,7 @@ public class ColumnInfo
         Property = property;
         Id = Property.Name;
         Name = Property.DisplayName();
+        Required = Property.IsRequired();
 
         var form = Property.GetCustomAttribute<FormAttribute>();
         if (form != null)
@@ -145,7 +146,6 @@ public class ColumnInfo
             Type = Utils.ConvertTo<FieldType>(form.Type);
             ReadOnly = form.ReadOnly;
             Placeholder = form.Placeholder;
-            Required = Property.IsRequired();
         }
 
         var code = Property.GetCustomAttribute<CategoryAttribute>();
