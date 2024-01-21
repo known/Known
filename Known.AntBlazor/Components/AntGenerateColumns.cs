13ab69767d3405060f7ecb671aafaf4095e0153c
KnownChen <knownchen@163.com>
2024-01-21 20:48:16 +0800

    暂不支持Ant固定列

--- Known.AntBlazor/Components/AntGenerateColumns.cs
+++ Known.AntBlazor/Components/AntGenerateColumns.cs
@@ -64,7 +64,8 @@ public class AntGenerateColumns<TItem> : BaseComponent where TItem : class, new(
         var title = Language?.GetString<TItem>(item);
         builder.AddAttribute(1, nameof(Column<TItem>.Title), title);
         builder.AddAttribute(1, nameof(Column<TItem>.Sortable), item.IsSort);
-        builder.AddAttribute(1, nameof(Column<TItem>.Fixed), item.Fixed);
+        //TODO:固定列显示混乱问题
+        //builder.AddAttribute(1, nameof(Column<TItem>.Fixed), item.Fixed);
         builder.AddAttribute(1, nameof(Column<TItem>.Width), item.Width);
         if (!string.IsNullOrWhiteSpace(item.DefaultSort))
         {
