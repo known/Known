3c6425698516d66718e42514d16a8cdc2849e51c
KnownChen <knownchen@163.com>
2024-02-28 17:23:08 +0800

    优化表单组件

---
+++ Known.AntBlazor/Components/AntForm.cs
@@ -0,0 +1,35 @@
+﻿using AntDesign;
+using Known.Blazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Known.AntBlazor.Components;
+
+public class AntForm<TItem> : Form<TItem> where TItem : class, new()
+{
+    [Parameter] public bool ShowAction { get; set; }
+    [Parameter] public FormModel<TItem> Form { get; set; }
+
+    protected override void OnInitialized()
+    {
+        ValidateOnChange = true;
+        ValidateMode = FormValidateMode.Rules;
+        LabelColSpan = Form?.LabelSpan ?? 0;
+        WrapperColSpan = Form?.WrapperSpan ?? 0;
+        Model = Form?.Data;
+        Form?.Initialize();
+        base.OnInitialized();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+            Form.OnValidate = Validate;
+        base.OnAfterRender(firstRender);
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        base.BuildRenderTree(builder);
+    }
+}
