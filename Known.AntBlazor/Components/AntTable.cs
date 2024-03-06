10ceea9cbfd82f7123277709aab59177adf0461a
KnownChen <knownchen@163.com>
2024-03-06 20:54:39 +0800

    添加AntTable组件

---
+++ Known.AntBlazor/Components/AntTable.cs
@@ -0,0 +1,68 @@
+﻿using AntDesign.TableModels;
+
+namespace Known.AntBlazor.Components;
+
+public class AntTable<TItem> : Table<TItem>
+{
+    [CascadingParameter] public Error Error { get; set; }
+
+    [Parameter] public Context Context { get; set; }
+    [Parameter] public PagingCriteria Criteria { get; set; }
+    [Parameter] public PagingResult<TItem> Result { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Size = TableSize.Small;
+        Responsive = true;
+        ScrollBarWidth = "8px";
+        if (Criteria != null)
+        {
+            PageIndex = Criteria.PageIndex;
+            PageIndexChanged = this.Callback<int>(v => Criteria.PageIndex = v);
+            PageSize = Criteria.PageSize;
+            PageSizeChanged = this.Callback<int>(v => Criteria.PageSize = v);
+        }
+        PaginationPosition = "bottomRight";
+        if (!OnChange.HasDelegate)
+            OnChange = this.Callback<QueryModel<TItem>>(OnDataChange);
+        PaginationTemplate = this.BuildTree<(int PageSize, int PageIndex, int Total, string PaginationClass, EventCallback<PaginationEventArgs> HandlePageChange)>(BuildPagination);
+        base.OnInitialized();
+    }
+
+    private void BuildPagination(RenderTreeBuilder builder, (int PageSize, int PageIndex, int Total, string PaginationClass, EventCallback<PaginationEventArgs> HandlePageChange) tuple)
+    {
+        Func<PaginationTotalContext, string> showTotal = ctx => Context?.Language["Page.Total"].Replace("{total}", $"{ctx.Total}");
+        builder.Component<Pagination>()
+               .Set(c => c.Class, tuple.PaginationClass)
+               .Set(c => c.Total, tuple.Total)
+               .Set(c => c.PageSize, tuple.PageSize)
+               .Set(c => c.Current, tuple.PageIndex)
+               .Set(c => c.ShowTotal, showTotal)
+               .Set(c => c.ShowSizeChanger, true)
+               .Set(c => c.ShowQuickJumper, true)
+               .Set(c => c.OnChange, tuple.HandlePageChange)
+               .Build();
+    }
+
+    protected override void OnParametersSet()
+    {
+        if (Result != null)
+        {
+            DataSource = Result.PageData;
+            Total = Result.TotalCount;
+        }
+        base.OnParametersSet();
+    }
+
+    private async void OnDataChange(QueryModel<TItem> queryModel)
+    {
+        try
+        {
+            
+        }
+        catch (Exception ex)
+        {
+            await Error.HandleAsync(ex);
+        }
+    }
+}
