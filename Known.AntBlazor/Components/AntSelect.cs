4ec72735e35acc02598071eb18c4eb9bc7ac6938
KnownChen <knownchen@163.com>
2024-03-07 16:10:33 +0800

    AntSelect添加Category参数

--- Known.AntBlazor/Components/AntSelect.cs
+++ Known.AntBlazor/Components/AntSelect.cs
@@ -5,6 +5,8 @@ public class AntSelect : Select<string, CodeInfo>
     [CascadingParameter] private IAntForm AntForm { get; set; }
     [CascadingParameter] private DataItem Item { get; set; }
 
+    [Parameter] public string Category { get; set; }
+
     protected override void OnInitialized()
 	{
         if (AntForm != null)
@@ -13,6 +15,9 @@ public class AntSelect : Select<string, CodeInfo>
             Item.Type = typeof(string);
         ValueName = nameof(CodeInfo.Code);
 		LabelName = nameof(CodeInfo.Name);
+        if (!string.IsNullOrWhiteSpace(Category))
+            DataSource = Cache.GetCodes(Category).ToCodes("请选择");
+        Placeholder = "请选择";
         base.OnInitialized();
 	}
 }
