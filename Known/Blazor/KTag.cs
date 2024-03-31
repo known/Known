81b9aaf185ab2356b0d468d9d1752e6163a278fd
KnownChen <knownchen@163.com>
2024-03-31 13:46:59 +0800

    添加KTag组件

---
+++ Known/Blazor/KTag.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Known.Blazor;
+
+public class KTag : BaseComponent
+{
+    [Parameter] public string Text { get; set; }
+
+    protected override void BuildRender(RenderTreeBuilder builder)
+    {
+        UI.BuildTag(builder, Text);
+    }
+}
