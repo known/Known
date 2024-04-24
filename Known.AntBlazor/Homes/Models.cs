567521722abff444690d2e533baf7fd7a107cf1d
KnownChen <knownchen@163.com>
2024-04-24 12:23:09 +0800

    初始化

--- Known.AntBlazor/Homes/Models.cs
+++ Known.AntBlazor/Homes/Models.cs
@@ -7,3 +7,18 @@ public class StatisticCountInfo
     public int? Count { get; set; }
     public string Url { get; set; }
 }
+
+public class ChartCardOption
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public List<CardChartInfo> Charts { get; set; } = [];
+}
+
+public class CardChartInfo
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public ChartDataInfo[] Datas { get; set; }
+}
