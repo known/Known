4e6db71bf6a56354273002207067440f28401aae
KnownChen <knownchen@163.com>
2024-04-29 11:38:41 +0800

    初始化

--- Known.AntBlazor/TypeColumns.cs
+++ Known.AntBlazor/TypeColumns.cs
@@ -1,3 +1,28 @@
 ﻿namespace Known.AntBlazor;
 
+public class StringColumn : Column<string> { }
+public class BooleanColumn : Column<bool> { }
+public class IntegerColumn : Column<int?> { }
+public class DecimalColumn : Column<decimal?> { }
+
+public class DateColumn : Column<DateTime?>
+{
+    public DateColumn()
+    {
+        Format = "yyyy-MM-dd";
+        Width = "120";
+        Align = ColumnAlign.Center;
+    }
+}
+
+public class DateTimeColumn : Column<DateTime?>
+{
+    public DateTimeColumn()
+    {
+        Format = "yyyy-MM-dd HH:ss";
+        Width = "150";
+        Align = ColumnAlign.Center;
+    }
+}
+
 public class DictionaryColumn : PropertyColumn<Dictionary<string, object>, object> { }
