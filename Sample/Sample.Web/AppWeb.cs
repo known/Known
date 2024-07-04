79d0f0612cfa32825c0ae5f53485d2ed58dd678e
KnownChen <knownchen@163.com>
2024-07-04 08:41:22 +0800

    初始化

--- Sample/Sample.Web/AppWeb.cs
+++ Sample/Sample.Web/AppWeb.cs
@@ -56,6 +56,26 @@ public static class AppWeb
             scheduler.Schedule<ImportTaskJob>().EveryFiveSeconds();
         });
     }
+
+    private static void AddSampleCore(this IServiceCollection services, Action<AppInfo> action = null)
+    {
+        services.AddKnownCore(info =>
+        {
+            //info.ProductId = "Test";
+            //info.CheckSystem = info => Result.Error("无效密钥，请重新授权！");
+            action?.Invoke(info);
+        });
+
+        services.AddScoped<IHomeService, HomeService>();
+        services.AddScoped<IApplyService, ApplyService>();
+
+        //注册待办事项显示流程表单
+        //Config.ShowMyFlow = flow =>
+        //{
+        //    if (flow.Flow.FlowCode == AppFlow.Apply.Code)
+        //        ApplyForm.ShowMyFlow(flow);
+        //};
+    }
 }
 
 class ImportTaskJob : IInvocable
