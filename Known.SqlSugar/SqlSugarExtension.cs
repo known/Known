4ee6682fb4db61e58f99453baf71b92192dd93c1
Known Chen <knownchen@163.com>
2024-08-16 16:45:06 +0800

    修改用户管理配置

---
+++ Known.SqlSugar/SqlSugarExtension.cs
@@ -0,0 +1,39 @@
+﻿namespace Known.SqlSugar;
+
+static class SqlSugarExtension
+{
+    internal static async Task<List<T>> QueryListAsync<T>(this SqlSugarScope sugar, string sql, List<SugarParameter> parameters)
+    {
+        using (var reader = await sugar.Ado.GetDataReaderAsync(sql, parameters))
+        {
+            return ConvertToList<T>(reader);
+        }
+    }
+
+    internal static async Task<T> QueryAsync<T>(this SqlSugarScope sugar, string sql, List<SugarParameter> parameters)
+    {
+        using (var reader = await sugar.Ado.GetDataReaderAsync(sql, parameters))
+        {
+            return ConvertTo<T>(reader);
+        }
+    }
+
+    private static T ConvertTo<T>(System.Data.IDataReader reader)
+    {
+        if (reader.Read())
+            return (T)DBUtils.ConvertTo<T>(reader);
+
+        return default;
+    }
+
+    private static List<T> ConvertToList<T>(System.Data.IDataReader reader)
+    {
+        var lists = new List<T>();
+        while (reader.Read())
+        {
+            var obj = DBUtils.ConvertTo<T>(reader);
+            lists.Add((T)obj);
+        }
+        return lists;
+    }
+}
---
+++ Sample/SqlSugarWeb/Program.cs
@@ -0,0 +1,59 @@
+﻿using Known.SqlSugar;
+using Sample.Web;
+using SqlSugar;
+
+#if DEBUG
+Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+#endif
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents();
+builder.Services.AddApp(info =>
+{
+    info.WebRoot = builder.Environment.WebRootPath;
+    info.ContentRoot = builder.Environment.ContentRootPath;
+});
+builder.Services.AddKnownSqlSugar(config =>
+{
+    config.DbType = DbType.MySql;
+    config.ConnectionString = builder.Configuration.GetSection("ConnString").Get<string>();
+    config.IsAutoCloseConnection = true;
+    config.MoreSettings ??= new ConnMoreSettings();
+    config.MoreSettings.IsAutoToUpper = false;
+    config.AopEvents ??= new AopEvents();
+    config.AopEvents.OnLogExecuting = (sql, pars) =>
+    {
+        var param = string.Join(",", pars.Select(p => $"{p.ParameterName}={p.Value}"));
+        Console.WriteLine($"SQL: {sql}");
+        Console.WriteLine($"参数: {param}");
+    };
+});
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    //app.UseCssLiveReload();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAntiforgery();
+app.UseApp();
+app.MapRazorPages();
+app.MapRazorComponents<SqlSugarWeb.App>()   
+   .AddInteractiveServerRenderMode()
+   .AddInteractiveWebAssemblyRenderMode()
+   .AddAdditionalAssemblies([.. Config.Assemblies]);
+app.Run();
