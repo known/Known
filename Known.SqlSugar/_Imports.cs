a79afecdcdaab5ddae3509658936dd901880b0af
Known Chen <knownchen@163.com>
2024-09-25 14:42:26 +0800

    Known.SqlSugar从项目独立出来一个新库

--- Known.SqlSugar/_Imports.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System.Linq.Expressions;
-global using Known.Data;
-global using Known.Entities;
-global using Known.WorkFlows;
-global using Microsoft.Extensions.DependencyInjection;
-global using SqlSugar;
