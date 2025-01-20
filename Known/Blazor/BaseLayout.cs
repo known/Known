a8cfc39141c7142ed0f5c9fb11356e972ba1737a
Known Chen <knownchen@163.com>
2025-01-20 13:21:04 +0800

    优化模板页

--- Known/Blazor/BaseLayout.cs
+++ Known/Blazor/BaseLayout.cs
@@ -5,7 +5,71 @@
 /// </summary>
 public class BaseLayout : BaseComponent
 {
+    private AdminInfo Info { get; set; }
     internal MenuInfo CurrentMenu => Context?.Current;
+
+    [CascadingParameter] private RouteData RouteData { get; set; }
+    [Inject] private IAuthStateProvider AuthProvider { get; set; }
+
+    /// <summary>
+    /// 取得或设置是否是管理后台模板。
+    /// </summary>
+    [Parameter] public bool IsAdmin { get; set; }
+
+    /// <summary>
+    /// 取得当前用户权限菜单列表。
+    /// </summary>
+    [Parameter] public List<MenuInfo> UserMenus { get; set; }
+
+    /// <inheritdoc />
+    protected override async Task OnInitAsync()
+    {
+        if (!IsAdmin)
+            return;
+
+        await base.OnInitAsync();
+        if (IsServerMode)
+            await InitAdminAsync();
+    }
+
+    /// <inheritdoc />
+    protected override async Task OnParameterAsync()
+    {
+        await base.OnParameterAsync();
+        Context.Url = Navigation.GetPageUrl();
+        Context.SetCurrentMenu(RouteData);
+        CheckUrlAuthentication();
+    }
+
+    /// <inheritdoc />
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        if (firstRender)
+        {
+            await OnThemeColorAsync();
+            if (Config.App.IsSize)
+            {
+                var size = await JS.GetCurrentSizeAsync();
+                if (string.IsNullOrWhiteSpace(size))
+                    size = Context.UserSetting.Size;
+                if (string.IsNullOrWhiteSpace(size))
+                    size = Config.App.DefaultSize;
+                await JS.SetCurrentSizeAsync(size);
+            }
+            if (Config.App.IsLanguage)
+            {
+                var language = await JS.GetCurrentLanguageAsync();
+                if (string.IsNullOrWhiteSpace(language))
+                    language = Context.UserSetting.Language;
+                Context.CurrentLanguage = language;
+            }
+
+            if (!IsServerMode)
+                await InitAdminAsync();
+            LoadMenus();
+        }
+    }
 
     /// <summary>
     /// 导航到指定菜单对应的页面。
@@ -37,101 +101,85 @@ public class BaseLayout : BaseComponent
     /// </summary>
     /// <param name="user">用户信息。</param>
     /// <returns></returns>
-    public virtual Task SignInAsync(UserInfo user) => Task.CompletedTask;
+    public async Task SignInAsync(UserInfo user)
+    {
+        if (AuthProvider != null)
+            await AuthProvider.SignInAsync(user);
+    }
 
     /// <summary>
     /// 异步注销，用户安全退出系统。
     /// </summary>
     /// <returns></returns>
-    public virtual Task SignOutAsync() => Task.CompletedTask;
+    public async Task SignOutAsync()
+    {
+        var result = await Admin.SignOutAsync();
+        if (result.IsValid)
+        {
+            Context.SignOut();
+            await SignInAsync(null);
+            Navigation?.GoLoginPage();
+            Config.OnExit?.Invoke();
+        }
+    }
 
     /// <summary>
     /// 重新加载当前页面，如果是多标签，则刷新当前标签页。
     /// </summary>
     public virtual void ReloadPage() { }
 
+    internal void Logout()
+    {
+        UI.Confirm(Language["Tip.Exits"], SignOutAsync);
+    }
+
+    internal virtual void LoadMenus() { }
     internal virtual void ToggleSide(bool collapsed) { }
-    internal virtual void AddMenuItem(MenuInfo item) { }
-}
 
-/// <summary>
-/// 空模板组件类。
-/// </summary>
-public class EmptyLayout : LayoutComponentBase
-{
-    /// <inheritdoc />
-    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    internal virtual void AddMenuItem(MenuInfo item)
     {
-        builder.Div("kui-wrapper", () => builder.Fragment(Body));
-    }
-}
-
-/// <summary>
-/// 管理后台模板页类。
-/// </summary>
-public class AdminLayout : LayoutComponentBase
-{
-    [CascadingParameter] private UIContext Context { get; set; }
-    [Inject] private IAuthStateProvider AuthProvider { get; set; }
-    [Inject] private IServiceScopeFactory Factory { get; set; }
-    [Inject] private NavigationManager Navigation { get; set; }
-    private IAdminService Admin { get; set; }
-    private bool IsLoaded { get; set; } = true;
-
-    /// <inheritdoc />
-    protected override async Task OnInitializedAsync()
-    {
-        await base.OnInitializedAsync();
-        Admin = await Factory.CreateAsync<IAdminService>(Context);
-
-        IsLoaded = false;
-        if (!Config.IsInstalled)
-        {
-            var isInstall = await Admin.GetIsInstallAsync(); //检查是否需要安装
-            Config.IsInstalled = !isInstall;
-        }
-        if (!Config.IsInstalled)
-        {
-            Navigation?.GoInstallPage();
-            return;
-        }
-
-        var user = await GetCurrentUserAsync();
-        if (user == null)
-        {
-            Navigation?.GoLoginPage();
-            return;
-        }
-
-        Context.CurrentUser = user;
-        IsLoaded = true;
+        if (Context.UserMenus == null)
+            Context.UserMenus = [];
+        Context.UserMenus.Add(item);
+        UserMenus = Context.UserMenus.ToMenuItems();
+        LoadMenus();
     }
 
-    /// <inheritdoc />
-    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    internal async Task OnThemeColorAsync()
     {
-        if (!IsLoaded)
+        var theme = Context.UserSetting.ThemeColor;
+        var href = $"_content/Known/css/theme/{theme}.css";
+        await JS.SetStyleSheetAsync("/theme/", href);
+    }
+
+    private async Task InitAdminAsync()
+    {
+        Info = await Admin.GetAdminAsync();
+        Context.UserSetting = Info?.UserSetting ?? new();
+        Context.UserTableSettings = Info?.UserTableSettings ?? [];
+        if (!Context.IsMobileApp)
+            SetUserMenus(Info?.UserMenus);
+        Cache.AttachCodes(Info?.Codes);
+    }
+
+    private void SetUserMenus(List<MenuInfo> menus)
+    {
+        Context.UserMenus = menus;
+        UserMenus = menus?.ToMenuItems();
+    }
+
+    private void CheckUrlAuthentication()
+    {
+        if (!IsAdmin)
             return;
 
-        builder.Div("kui-wrapper", () =>
+        if (!UIConfig.IgnoreRoutes.Contains(Context.Url) && !RouteData.PageType.IsAllowAnonymous())
         {
-            if (UIConfig.AdminBody != null)
-                UIConfig.AdminBody.Invoke(builder, BuildBody);
-            else
-                builder.Component<KLayout>().Set(c => c.IsAdmin, true).Set(c => c.ChildContent, BuildBody).Build();
-        });
-    }
-
-    private void BuildBody(RenderTreeBuilder builder)
-    {
-        builder.Component<AuthPanel>().Set(c => c.ChildContent, Body).Build();
-    }
-
-    private async Task<UserInfo> GetCurrentUserAsync()
-    {
-        if (AuthProvider == null)
-            return null;
-
-        return await AuthProvider.GetUserAsync();
+            if (Context.Current == null)
+            {
+                Navigation.GoErrorPage("403");
+                return;
+            }
+        }
     }
 }
---
+++ Known/Blazor/Layouts.cs
@@ -0,0 +1,102 @@
+﻿namespace Known.Blazor;
+
+/// <summary>
+/// 空模板组件类。
+/// </summary>
+public class EmptyLayout : LayoutComponentBase
+{
+    /// <inheritdoc />
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.Div("kui-wrapper", () => builder.Fragment(Body));
+    }
+}
+
+/// <summary>
+/// 管理后台模板页类。
+/// </summary>
+public class AdminLayout : LayoutComponentBase
+{
+    [CascadingParameter] private UIContext Context { get; set; }
+    [Inject] private IAuthStateProvider AuthProvider { get; set; }
+    [Inject] private IServiceScopeFactory Factory { get; set; }
+    [Inject] private NavigationManager Navigation { get; set; }
+    [Inject] private JSService JS { get; set; }
+    private IAdminService Admin { get; set; }
+    private bool IsLoaded { get; set; } = true;
+
+    /// <inheritdoc />
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        Admin = await Factory.CreateAsync<IAdminService>(Context);
+
+        IsLoaded = false;
+        if (!Config.IsInstalled)
+        {
+            var isInstall = await Admin.GetIsInstallAsync(); //检查是否需要安装
+            Config.IsInstalled = !isInstall;
+        }
+        if (!Config.IsInstalled)
+        {
+            Navigation?.GoInstallPage();
+            return;
+        }
+
+        var user = await GetCurrentUserAsync();
+        if (user == null)
+        {
+            Navigation?.GoLoginPage();
+            return;
+        }
+
+        Context.CurrentUser = user;
+        IsLoaded = true;
+    }
+
+    /// <inheritdoc />
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        if (firstRender)
+        {
+            await JS.InitFilesAsync();
+        }
+    }
+
+    /// <inheritdoc />
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        if (!IsLoaded)
+            return;
+
+        builder.Div("kui-wrapper", () =>
+        {
+            builder.Component<KLayout>()
+                   .Set(c => c.IsAdmin, true)
+                   .Set(c => c.ChildContent, BuildLayout)
+                   .Build();
+        });
+    }
+
+    private void BuildLayout(RenderTreeBuilder builder)
+    {
+        if (UIConfig.AdminBody != null)
+            UIConfig.AdminBody.Invoke(builder, BuildBody);
+        else
+            builder.Component<MainLayout>().Set(c => c.ChildContent, BuildBody).Build();
+    }
+
+    private void BuildBody(RenderTreeBuilder builder)
+    {
+        builder.Component<AuthPanel>().Set(c => c.ChildContent, Body).Build();
+    }
+
+    private async Task<UserInfo> GetCurrentUserAsync()
+    {
+        if (AuthProvider == null)
+            return null;
+
+        return await AuthProvider.GetUserAsync();
+    }
+}
