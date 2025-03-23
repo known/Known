609f43e45091ad4031bec15190232b5e62e5a6bc
Known Chen <knownchen@163.com>
2025-03-23 20:10:13 +0800

    初始化

---
+++ Shared/WorkFlows/FlowExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace Known.WorkFlows;
+
+/// <summary>
+/// 工作流扩展类。
+/// </summary>
+public static partial class FlowExtension
+{
+    /// <summary>
+    /// 判断流程是否可以提交。
+    /// </summary>
+    /// <param name="entity">流程实体对象。</param>
+    /// <returns></returns>
+    public static bool CanSubmit(this FlowEntity entity)
+    {
+        return entity.BizStatus == FlowStatus.Save ||
+               entity.BizStatus == FlowStatus.Revoked ||
+               entity.BizStatus == FlowStatus.VerifyFail ||
+               entity.BizStatus == FlowStatus.Reapply;
+    }
+
+    /// <summary>
+    /// 判断流程是否可以撤回。
+    /// </summary>
+    /// <param name="entity">流程实体对象。</param>
+    /// <returns></returns>
+    public static bool CanRevoke(this FlowEntity entity)
+    {
+        return entity.BizStatus == FlowStatus.Verifing;
+    }
+}
