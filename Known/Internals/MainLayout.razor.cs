651ab72eb41414ff43a106e77ff2c34c090a9294
Known Chen <knownchen@163.com>
2025-03-31 08:41:50 +0800

    优化面包屑首页点击事件

---
+++ Known/Internals/MainLayout.razor.cs
@@ -0,0 +1,63 @@
+﻿namespace Known.Internals;
+
+/// <summary>
+/// 主模板组件类。
+/// </summary>
+public partial class MainLayout
+{
+    private bool collapsed = false;
+
+    private KLayout layout;
+    private MainMenu menu;
+    private MainBody body;
+    private MenuInfo root;
+
+    private string LayoutClass => CssBuilder.Default("kui-layout").AddClass(Context.UserSetting.Size).BuildClass();
+    private string HeaderClass => CssBuilder.Default("kui-header")
+                                            .AddClass("kui-menu-dark", Context.UserSetting.MenuTheme == "Dark")
+                                            .BuildClass();
+    private string MenuClass => CssBuilder.Default()
+                                          .AddClass("kui-menu-dark", Context.UserSetting.MenuTheme == "Dark")
+                                          .AddClass("kui-menu-float", Context.UserSetting.LayoutMode == LayoutMode.Float.ToString())
+                                          .BuildClass();
+
+    /// <summary>
+    /// 取得或设置子组件内容。
+    /// </summary>
+    [Parameter] public RenderFragment ChildContent { get; set; }
+
+    /// <inheritdoc />
+    protected override Task OnInitAsync()
+    {
+        collapsed = Context.IsMobile;
+        return base.OnInitAsync();
+    }
+
+    private void OnLoadMenus(List<MenuInfo> menus)
+    {
+        root = Config.App.GetRootMenu();
+        root.AddChildren(menus);
+        menu?.SetData(root);
+    }
+
+    private void OnReloadPage()
+    {
+        body?.ReloadPage();
+    }
+
+    private void OnToggle(bool collapsed)
+    {
+        this.collapsed = collapsed;
+        StateChanged();
+    }
+
+    private void OnLogoClick()
+    {
+        Navigation.GoHomePage();
+    }
+
+    private void OnSetting()
+    {
+        layout?.ShowSetting();
+    }
+}
---
+++ Known/Internals/TopBreadcrumb.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Known.Internals;
+
+/// <summary>
+/// 顶部面包屑组件类。
+/// </summary>
+public partial class TopBreadcrumb
+{
+    /// <summary>
+    /// 取得或设置当前菜单信息。
+    /// </summary>
+    [Parameter] public MenuInfo Current { get; set; }
+
+    /// <summary>
+    /// 取得或设置首页点击委托。
+    /// </summary>
+    [Parameter] public EventCallback OnHome { get; set; }
+
+    private void OnHomeClick()
+    {
+        if (OnHome.HasDelegate)
+            OnHome.InvokeAsync();
+        else
+            Navigation.GoHomePage();
+    }
+}
