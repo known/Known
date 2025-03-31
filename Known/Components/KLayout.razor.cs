116c2e7312061034b78aeead0b520050a5200acf
Known Chen <knownchen@163.com>
2025-03-31 09:02:31 +0800

    优化系统设置抽屉表单

---
+++ Known/Components/KLayout.razor.cs
@@ -0,0 +1,84 @@
+﻿namespace Known.Components;
+
+/// <summary>
+/// 布局组件类。
+/// </summary>
+public partial class KLayout
+{
+    /// <summary>
+    /// 取得或设置子组件内容。
+    /// </summary>
+    [Parameter] public RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// 取得或设置加载按钮委托。
+    /// </summary>
+    [Parameter] public Action<List<MenuInfo>> OnLoadMenus { get; set; }
+
+    /// <summary>
+    /// 取得或设置重新加载页面委托。
+    /// </summary>
+    [Parameter] public Action OnReloadPage { get; set; }
+
+    /// <inheritdoc />
+    public override async Task ShowSpinAsync(string text, Func<Task> action)
+    {
+        if (action == null)
+            return;
+
+        await JS.ShowSpinAsync(text);
+        await Task.Run(async () =>
+        {
+            try
+            {
+                await action?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                await OnErrorAsync(ex);
+            }
+            await JS.HideSpinAsync();
+        });
+    }
+
+    /// <inheritdoc />
+    public override void ReloadPage()
+    {
+        OnReloadPage?.Invoke();
+    }
+
+    /// <summary>
+    /// 显示设置表单。
+    /// </summary>
+    public void ShowSetting()
+    {
+        UI.ShowDrawer(new DrawerModel
+        {
+            Title = Language["Nav.Setting"],
+            Width = "340",
+            Content = b => b.Component<SettingForm>()
+                            .Set(c => c.OnSave, OnSaveSetting)
+                            .Set(c => c.OnReset, OnResetSetting)
+                            .Set(c => c.OnChange, StateChanged)
+                            .Build()
+        });
+    }
+
+    internal override void LoadMenus()
+    {
+        OnLoadMenus?.Invoke(UserMenus);
+    }
+
+    private async Task OnSaveSetting()
+    {
+        var result = await Admin.SaveUserSettingAsync(Context.UserSetting);
+        if (result.IsValid)
+            await OnThemeColorAsync();
+    }
+
+    private Task OnResetSetting()
+    {
+        Context.UserSetting = new();
+        return OnSaveSetting();
+    }
+}
---
+++ Known/Internals/SettingForm.razor.cs
@@ -0,0 +1,57 @@
+﻿namespace Known.Internals;
+
+public partial class SettingForm
+{
+    private readonly Dictionary<string, string> Colors = new()
+    {
+        ["default"] = "#1890ff",
+        ["dust"] = "#F5222D",
+        ["volcano"] = "#FA541C",
+        ["sunset"] = "#FAAD14",
+        ["cyan"] = "#13C2C2",
+        ["green"] = "#52C41A",
+        ["geekblue"] = "#2F54EB",
+        ["purple"] = "#722ED1"
+    };
+    private List<CodeInfo> MenuThemes => [
+        new CodeInfo("Dark", Language["Set.Dark"]),
+        new CodeInfo("Light", Language["Set.Light"])
+    ];
+    private List<CodeInfo> LayoutModes => [
+        new CodeInfo("Vertical", Language["Set.Vertical"]),
+        new CodeInfo("Horizontal", Language["Set.Horizontal"]),
+        new CodeInfo("Float", Language["Set.Float"])
+    ];
+
+    /// <summary>
+    /// 取得或设置系统设置项目改变事件委托。
+    /// </summary>
+    [Parameter] public Action OnChange { get; set; }
+
+    /// <summary>
+    /// 取得或设置保存系统设置委托。
+    /// </summary>
+    [Parameter] public Func<Task> OnSave { get; set; }
+
+    /// <summary>
+    /// 取得或设置重置系统设置委托。
+    /// </summary>
+    [Parameter] public Func<Task> OnReset { get; set; }
+
+    private async Task OnThemeColor(string theme)
+    {
+        Context.UserSetting.ThemeColor = theme;
+        var href = $"_content/Known/css/theme/{theme}.css";
+        await JS.SetStyleSheetAsync("/theme/", href);
+    }
+
+    private async Task OnResetAsync()
+    {
+        if (OnReset != null)
+        {
+            await OnReset.Invoke();
+            await StateChangedAsync();
+            OnChange?.Invoke();
+        }
+    }
+}
