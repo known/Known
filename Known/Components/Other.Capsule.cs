883057b0c598a93c40104bd5fde27b54a8dd2b85
Known Chen <knownchen@163.com>
2025-04-07 20:12:57 +0800

    添加胶囊组件

---
+++ Known/Components/Other.Capsule.cs
@@ -0,0 +1,27 @@
+﻿namespace Known.Components;
+
+/// <summary>
+/// 胶囊组件类。
+/// </summary>
+public class KCapsule : BaseComponent
+{
+    /// <summary>
+    /// 取得或设置胶囊组件的值。
+    /// </summary>
+    [Parameter] public string Value { get; set; }
+
+    /// <summary>
+    /// 取得或设置胶囊组件值的背景颜色。
+    /// </summary>
+    [Parameter] public string Color { get; set; }
+
+    /// <inheritdoc />
+    protected override void BuildRender(RenderTreeBuilder builder)
+    {
+        builder.Div("kui-capsule", () =>
+        {
+            builder.Span(Name);
+            builder.Span().Class("kui-capsule-value").Style($"background-color:{Color};").Child(Value);
+        });
+    }
+}
