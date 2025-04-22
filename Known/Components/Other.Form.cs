bb048b0c490481bfb0b9a87d53f1a23c789f534c
Known Chen <knownchen@163.com>
2025-04-22 14:54:03 +0800

    添加表单组件

---
+++ Known/Components/Other.Form.cs
@@ -0,0 +1,35 @@
+﻿namespace Known.Components;
+
+/// <summary>
+/// 表单组件类。
+/// </summary>
+public class KForm : BaseComponent
+{
+    private string ClassName => CssBuilder.Default("kui-form")
+                                          .AddClass(Class)
+                                          .AddClass("small-label", SmallLabel)
+                                          .AddClass("small-row", SmallRow)
+                                          .AddClass("readonly", ReadOnly)
+                                          .BuildClass();
+
+    /// <summary>
+    /// 取得或设置是否是窄宽标题列。
+    /// </summary>
+    [Parameter] public bool SmallLabel { get; set; }
+
+    /// <summary>
+    /// 取得或设置是否是窄宽行高。
+    /// </summary>
+    [Parameter] public bool SmallRow { get; set; }
+
+    /// <summary>
+    /// 取得或设置子组件模板。
+    /// </summary>
+    [Parameter] public RenderFragment ChildContent { get; set; }
+
+    /// <inheritdoc />
+    protected override void BuildRender(RenderTreeBuilder builder)
+    {
+        builder.Div(ClassName, () => builder.Fragment(ChildContent));
+    }
+}
--- Known/Components/Other.Tabs.cs
+++ Known/Components/Other.Tabs.cs
@@ -5,7 +5,7 @@ namespace Known.Components;
 /// <summary>
 /// 多标签表单，扩展AntDesign的Tabs组件。
 /// </summary>
-public class TabForm : Tabs
+public class KTabForm : Tabs
 {
     /// <inheritdoc />
     protected override void OnInitialized()
