00396a466adf38d0dbbfcc82bfcbf5627b81bd65
Known Chen <knownchen@163.com>
2025-06-22 16:25:07 +0800

    添加echarts.js

--- Known/Extension.cs
+++ Known/Extension.cs
@@ -140,6 +140,7 @@ public static partial class Extension
     {
         KScript.AddScript("_content/AntDesign/js/ant-design-blazor.js");
         KScript.AddScript("_content/Known/js/libs/jquery.js");
+        KScript.AddScript("_content/Known/js/libs/echarts.js");
         KScript.AddScript("_content/Known/js/libs/pdfobject.js");
         KScript.AddScript("_content/Known/js/libs/barcode.js");
         KScript.AddScript("_content/Known/js/libs/qrcode.js");
