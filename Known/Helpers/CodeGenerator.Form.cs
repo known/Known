c62aba54667f1696374a4a66de17d8c0593db843
Known Chen <knownchen@163.com>
2025-07-01 08:58:39 +0800

    AntDesign升级到1.4.2

--- Known/Helpers/CodeGenerator.Form.cs
+++ Known/Helpers/CodeGenerator.Form.cs
@@ -59,7 +59,7 @@ partial class CodeGenerator
         if (item.Type != FieldType.File)
             sb.AppendLine("            <{0} @bind-Value=\"@context.{1}\" />", control, item.Id);
         else
-            sb.AppendLine("            <KUpload Value=\"@context.{0}\" OnFilesChanged=\"@(files=>OnFilesChanged(nameof({0}.{1}), files))\" />", modelName, item.Id);
+            sb.AppendLine("            <KUpload Value=\"@context.{1}\" OnFilesChanged=\"@(files=>OnFilesChanged(nameof({0}.{1}), files))\" />", modelName, item.Id);
         sb.AppendLine("        </DataItem>");
     }
 
