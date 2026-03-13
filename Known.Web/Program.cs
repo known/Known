986b72f5587881489f8f49d519daa6fda0a4d426
Known Chen <knownchen@163.com>
2026-03-13 12:50:28 +0800

    初始化

---
+++ Known.Web/Program.cs
@@ -0,0 +1,20 @@
+using Known.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+
+app.Run();
