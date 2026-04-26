a712678fa76d08581ae09fe41f2abd5e41bd27ed
Known Chen <knownchen@163.com>
2026-04-26 17:08:35 +0800

    优化AI组件

--- Known/Extensions/UIExtension.cs
+++ Known/Extensions/UIExtension.cs
@@ -72,13 +72,13 @@ public static class UIExtension
     /// <summary>
     /// 显示AI聊天抽屉。
     /// </summary>
-    /// <typeparam name="TComponent">抽屉组件类型。</typeparam>
+    /// <typeparam name="T">抽屉组件类型。</typeparam>
     /// <param name="service">UI服务实例。</param>
     /// <param name="title">抽屉标题。</param>
     /// <param name="width">抽屉宽度。</param>
-    /// <param name="parameters">组件参数委托。</param>
-    public static void ShowAIDrawer<TComponent>(this UIService service, string title, string width = "700px", Action<ComponentBuilder<TComponent>> parameters = null)
-        where TComponent : Microsoft.AspNetCore.Components.IComponent
+    /// <param name="action">组件参数委托。</param>
+    public static void ShowAIDrawer<T>(this UIService service, string title, string width = "700px", Action<ComponentBuilder<T>> action = null)
+        where T : Microsoft.AspNetCore.Components.IComponent
     {
         var model = new DrawerModel
         {
@@ -86,12 +86,7 @@ public static class UIExtension
             Title = title,
             Width = width,
             MaskClosable = false,
-            Content = b =>
-            {
-                var component = b.Component<TComponent>();
-                parameters?.Invoke(component);
-                component.Build();
-            }
+            Content = b => b.Component(action)
         };
         service.ShowDrawer(model);
     }
