67476f94000d63f205f83cd36c1fb8c80f9575d1
Known Chen <knownchen@163.com>
2026-05-18 17:26:50 +0800

    优化简洁边栏母版

--- Known.Server/AppConfig.cs
+++ Known.Server/AppConfig.cs
@@ -34,7 +34,7 @@ public static class AppConfig
             info.Name = AppName;
             info.IsPlatform = true;
             info.IsTopMenu = true;
-            //info.IsSemiLayout = true;
+            //info.Layout = LayoutType.Semi;
         });
         services.AddSample();
         services.ConfigUI();
--- Known/Layouts/SideLayout.razor.cs
+++ Known/Layouts/SideLayout.razor.cs
@@ -5,8 +5,35 @@
 /// </summary>
 public partial class SideLayout
 {
+    private KLayout layout;
+    private MainMenu menu;
+    private MainBody body;
+    private MenuInfo root;
+
     /// <summary>
     /// 取得或设置子组件内容。
     /// </summary>
     [Parameter] public RenderFragment ChildContent { get; set; }
+
+    private void OnLoadMenus(List<MenuInfo> menus)
+    {
+        root = Config.App.GetRootMenu();
+        root.AddChildren(menus);
+        menu?.SetData(root);
+    }
+
+    private void OnReloadPage()
+    {
+        body?.ReloadPage();
+    }
+
+    private void OnLogoClick()
+    {
+        Context.GoHomePage();
+    }
+
+    private void OnSetting()
+    {
+        layout?.ShowSetting();
+    }
 }
