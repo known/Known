85681b490a6893db5777fd5e3d673584a0973d24
Known Chen <knownchen@163.com>
2026-05-20 12:10:41 +0800

    修复面包屑刷新问题

--- Known/Internals/TopBreadcrumb.razor.cs
+++ Known/Internals/TopBreadcrumb.razor.cs
@@ -1,4 +1,6 @@
-﻿namespace Known.Internals;
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace Known.Internals;
 
 /// <summary>
 /// 顶部面包屑组件类。
@@ -10,11 +12,30 @@ public partial class TopBreadcrumb
     /// </summary>
     [Parameter] public EventCallback OnHome { get; set; }
 
+    /// <inheritdoc />
+    protected override Task OnInitAsync()
+    {
+        Navigation.LocationChanged += OnLocationChanged;
+        return base.OnInitAsync();
+    }
+
+    /// <inheritdoc />
+    protected override Task OnDisposeAsync()
+    {
+        Navigation.LocationChanged -= OnLocationChanged;
+        return base.OnDisposeAsync();
+    }
+
     private void OnHomeClick()
     {
         if (OnHome.HasDelegate)
             OnHome.InvokeAsync();
         else
             Context.GoHomePage();
+    }
+
+    private void OnLocationChanged(object sender, LocationChangedEventArgs e)
+    {
+        InvokeAsync(StateHasChanged);
     }
 }
